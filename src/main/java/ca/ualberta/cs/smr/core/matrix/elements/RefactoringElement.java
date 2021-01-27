e65c70f7c266569ec2c1476fd4e599f727564fa0
Max Ellis <mjellis@ualberta.ca>
2021-01-26 17:20:04 -0800

    Make RefactoringElement abstract

--- src/main/java/ca/ualberta/cs/smr/core/matrix/elements/RefactoringElement.java
+++ src/main/java/ca/ualberta/cs/smr/core/matrix/elements/RefactoringElement.java
@@ -3,13 +3,9 @@ package ca.ualberta.cs.smr.core.matrix.elements;
 import ca.ualberta.cs.smr.core.matrix.visitors.Visitor;
 import org.refactoringminer.api.Refactoring;
 
-public class RefactoringElement implements Element {
+public abstract class RefactoringElement implements Element {
     Refactoring elementRef;
 
-    @Override
-    public void accept(Visitor v) {
-        v.visit();
-    }
 
     public void set(Refactoring ref) {
         elementRef = ref;
