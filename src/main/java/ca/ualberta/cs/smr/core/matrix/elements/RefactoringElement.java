677c7a39175b813efb6647a5c022bbe3c0629d33
Max Ellis <mjellis@ualberta.ca>
2021-01-26 18:25:34 -0800

    Remove unnecessary imports

--- src/main/java/ca/ualberta/cs/smr/core/matrix/elements/RefactoringElement.java
+++ src/main/java/ca/ualberta/cs/smr/core/matrix/elements/RefactoringElement.java
@@ -1,6 +1,6 @@
 package ca.ualberta.cs.smr.core.matrix.elements;
 
-import ca.ualberta.cs.smr.core.matrix.visitors.Visitor;
+
 import org.refactoringminer.api.Refactoring;
 
 public abstract class RefactoringElement implements Element {
