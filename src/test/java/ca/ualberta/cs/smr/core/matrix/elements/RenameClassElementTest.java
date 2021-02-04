80cadd5c11e232c807e7ea983e8a71d47d05a962
Max Ellis <mjellis@ualberta.ca>
2021-02-03 16:03:02 -0800

    Test accept in each element

--- src/test/java/ca/ualberta/cs/smr/core/matrix/elements/RenameClassElementTest.java
+++ src/test/java/ca/ualberta/cs/smr/core/matrix/elements/RenameClassElementTest.java
@@ -1,13 +1,26 @@
 package ca.ualberta.cs.smr.core.matrix.elements;
 
 import ca.ualberta.cs.smr.GetDataForTests;
+import ca.ualberta.cs.smr.core.matrix.visitors.RenameMethodVisitor;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.refactoringminer.api.Refactoring;
+
 
 import java.util.List;
 
+import static org.mockito.Mockito.times;
+
 public class RenameClassElementTest {
+
+    @Test
+    public void testAccept() {
+        RenameClassElement element = Mockito.mock(RenameClassElement.class);
+        RenameMethodVisitor visitor = Mockito.mock(RenameMethodVisitor.class);
+        element.accept(visitor);
+        Mockito.verify(element, times(1)).accept(visitor);
+    }
 
     @Test
     public void testSet() {
--- src/test/java/ca/ualberta/cs/smr/core/matrix/elements/RenameMethodElementTest.java
+++ src/test/java/ca/ualberta/cs/smr/core/matrix/elements/RenameMethodElementTest.java
@@ -1,13 +1,25 @@
 package ca.ualberta.cs.smr.core.matrix.elements;
 
 import ca.ualberta.cs.smr.GetDataForTests;
+import ca.ualberta.cs.smr.core.matrix.visitors.RenameMethodVisitor;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.refactoringminer.api.Refactoring;
 
 import java.util.List;
 
+import static org.mockito.Mockito.times;
+
 public class RenameMethodElementTest {
+
+    @Test
+    public void testAccept() {
+        RenameMethodElement element = Mockito.mock(RenameMethodElement.class);
+        RenameMethodVisitor visitor = Mockito.mock(RenameMethodVisitor.class);
+        element.accept(visitor);
+        Mockito.verify(element, times(1)).accept(visitor);
+    }
 
     @Test
     public void testSet() {
@@ -21,5 +33,6 @@ public class RenameMethodElementTest {
         element.set(ref, basePath);
         Assert.assertNotNull("The refactoring element should not be null", element.elementRef);
         Assert.assertEquals("The set path should be the same as the base path", element.path, basePath);
+
     }
 }
