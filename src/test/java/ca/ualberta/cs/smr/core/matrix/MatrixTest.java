008551cdf454f940dcb99e866f3334155a3a726e
Max Ellis <mjellis@ualberta.ca>
2021-02-04 13:15:05 -0800

    Test makeElement and makeVisitor

--- src/test/java/ca/ualberta/cs/smr/core/matrix/MatrixTest.java
+++ src/test/java/ca/ualberta/cs/smr/core/matrix/MatrixTest.java
@@ -1,5 +1,6 @@
 package ca.ualberta.cs.smr.core.matrix;
 
+import ca.ualberta.cs.smr.GetDataForTests;
 import ca.ualberta.cs.smr.core.matrix.elements.RefactoringElement;
 import ca.ualberta.cs.smr.core.matrix.elements.RenameClassElement;
 import ca.ualberta.cs.smr.core.matrix.elements.RenameMethodElement;
@@ -8,9 +9,18 @@ import ca.ualberta.cs.smr.core.matrix.visitors.RenameClassVisitor;
 import ca.ualberta.cs.smr.core.matrix.visitors.RenameMethodVisitor;
 import org.junit.Assert;
 import org.junit.Test;
+import org.refactoringminer.api.Refactoring;
 import org.refactoringminer.api.RefactoringType;
 
+import java.util.List;
+
 public class MatrixTest {
+
+    @Test
+    public void testMatrixConstructor() {
+        Matrix matrix = new Matrix("Path");
+        Assert.assertEquals("Path", matrix.path);
+    }
 
     @Test
     public void testElementMap() {
@@ -37,6 +47,38 @@ public class MatrixTest {
         type = RefactoringType.RENAME_METHOD;
         visitor = Matrix.visitorMap.get(type);
         equals = visitor.getClass().equals(renameMethodVisitor.getClass());
+        Assert.assertTrue(equals);
+    }
+
+    @Test
+    public void testMakeElement() {
+        String basePath = System.getProperty("user.dir");
+        String originalPath = basePath + "/src/test/resources/original/MethodOverloadConflict";
+        String refactoredPath = basePath + "/src/test/resources/refactored/MethodOverloadConflict";
+        List<Refactoring> refactorings = GetDataForTests.getRefactorings("RENAME_METHOD", originalPath, refactoredPath);
+        assert refactorings != null;
+        Refactoring ref = refactorings.get(0);
+        RenameMethodElement mockElement = new RenameMethodElement();
+        Matrix matrix = new Matrix(basePath);
+        RefactoringElement element = matrix.makeElement(ref.getRefactoringType(), ref);
+        boolean equals = element.getClass().equals(mockElement.getClass());
+        Assert.assertTrue(equals);
+
+
+    }
+
+    @Test
+    public void testMakeVisitor() {
+        String basePath = System.getProperty("user.dir");
+        String originalPath = basePath + "/src/test/resources/original/MethodOverloadConflict";
+        String refactoredPath = basePath + "/src/test/resources/refactored/MethodOverloadConflict";
+        List<Refactoring> refactorings = GetDataForTests.getRefactorings("RENAME_METHOD", originalPath, refactoredPath);
+        assert refactorings != null;
+        Refactoring ref = refactorings.get(0);
+        RenameMethodVisitor mockVisitor = new RenameMethodVisitor();
+        Matrix matrix = new Matrix(basePath);
+        RefactoringVisitor visitor = matrix.makeVisitor(ref.getRefactoringType(), ref);
+        boolean equals = visitor.getClass().equals(mockVisitor.getClass());
         Assert.assertTrue(equals);
     }
 }
