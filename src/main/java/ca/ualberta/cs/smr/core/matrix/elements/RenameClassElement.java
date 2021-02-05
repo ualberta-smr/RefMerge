3feb18a1019ae3440f562060ef07c9fee10fc16e
Max Ellis <mjellis@ualberta.ca>
2021-02-05 13:32:45 -0800

    Refactor conflictHandlers package to conflictCheckers

--- src/main/java/ca/ualberta/cs/smr/core/matrix/elements/RenameClassElement.java
+++ src/main/java/ca/ualberta/cs/smr/core/matrix/elements/RenameClassElement.java
@@ -1,7 +1,7 @@
 package ca.ualberta.cs.smr.core.matrix.elements;
 
 
-import ca.ualberta.cs.smr.core.matrix.logicHandlers.ConflictCheckers;
+import ca.ualberta.cs.smr.core.matrix.conflictCheckers.ConflictCheckers;
 import ca.ualberta.cs.smr.core.matrix.visitors.Visitor;
 import org.refactoringminer.api.Refactoring;
 
