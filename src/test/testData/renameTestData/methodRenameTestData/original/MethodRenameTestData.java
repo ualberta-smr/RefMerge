4e003a96d6da4829f39843879569df8ce39855c6
Max Ellis <mjellis@ualberta.ca>
2021-02-10 18:46:07 -0800

    Add testData for undoRenameMethod test

---
+++ src/test/testData/renameTestData/methodRenameTestData/original/MethodRenameTestData.java
@@ -0,0 +1,26 @@
+package renameTestData.methodRenameTestData;
+
+
+public class MethodRenameTestData {
+
+    public void foo() {
+        int val = 5;
+        int y = val * val;
+        graph(val, y);
+    }
+
+    public void foo(int x) {
+        int y = x + x;
+        System.out.println(x + " " + y);
+        doStuff();
+    }
+
+    public void graph(int x, int y) {
+        System.out.println(x + " " + y);
+    }
+
+    public void doStuff() {
+        System.out.println("Doing stuff");
+    }
+
+}
