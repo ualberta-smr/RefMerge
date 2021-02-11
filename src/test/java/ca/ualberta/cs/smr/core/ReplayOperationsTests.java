f4c3fe8cb8f375b69820c4a1d5e87b84ea0fa541
Max Ellis <mjellis@ualberta.ca>
2021-02-11 13:22:54 -0800

    Add test for replayRenameMethod

---
+++ src/test/java/ca/ualberta/cs/smr/core/ReplayOperationsTests.java
@@ -0,0 +1,52 @@
+package ca.ualberta.cs.smr.core;
+
+import ca.ualberta.cs.smr.testUtils.GetDataForTests;
+import ca.ualberta.cs.smr.testUtils.TestUtils;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiMethod;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
+import org.refactoringminer.api.Refactoring;
+
+import java.util.List;
+
+public class ReplayOperationsTests extends LightJavaCodeInsightFixtureTestCase {
+
+    @Override
+    protected String getTestDataPath() {
+        return "src/test/testData";
+    }
+
+    public void testReplayRenameMethod() {
+        Project project = myFixture.getProject();
+        String testDataRenamed = "renameTestData/methodRenameTestData/renamed/";
+        String testDataOriginal = "renameTestData/methodRenameTestData/original/";
+        String testResult = "renameTestData/methodRenameTestData/expectedReplayResults/";
+        String testFile ="MethodRenameTestData.java";
+        PsiFile[] psiFiles = myFixture.configureByFiles(testDataOriginal + testFile, testResult + testFile);
+        String basePath = System.getProperty("user.dir");
+        String refactoredPath = basePath + "/" + getTestDataPath() + "/" + testDataRenamed;
+        String originalPath = basePath + "/" + getTestDataPath() + "/" + testDataOriginal;
+
+
+        PsiMethod[] oldMethods = TestUtils.getPsiMethodsFromFile(psiFiles[0]);
+        PsiMethod[] newMethods = TestUtils.getPsiMethodsFromFile(psiFiles[1]);
+
+        List<String> list1 = TestUtils.getMethodNames(oldMethods);
+        List<String> list2 = TestUtils.getMethodNames(newMethods);
+
+        LightJavaCodeInsightFixtureTestCase.assertNotSame(list1, list2);
+
+        List<Refactoring> refactorings = GetDataForTests.getRefactorings("RENAME_METHOD", originalPath, refactoredPath);
+        assert refactorings != null;
+        Refactoring ref = refactorings.get(0);
+        ReplayOperations replay = new ReplayOperations(project);
+        replay.replayRenameMethod(ref);
+
+
+        list1 = TestUtils.getMethodNames(oldMethods);
+        list2 = TestUtils.getMethodNames(newMethods);
+
+        LightJavaCodeInsightFixtureTestCase.assertSameElements(list1, list2);
+    }
+}
