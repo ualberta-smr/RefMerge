a81c1b33d17b0f1a3809c59d68e359be094bc0dd
Max Ellis <mjellis@ualberta.ca>
2021-03-02 17:58:29 -0800

    Add comparator for ordering based on refactoring type.

--- src/main/java/ca/ualberta/cs/smr/utils/sortingUtils/SortPairs.java
+++ src/main/java/ca/ualberta/cs/smr/utils/sortingUtils/SortPairs.java
@@ -1,6 +1,5 @@
 package ca.ualberta.cs.smr.utils.sortingUtils;
 
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -13,12 +12,4 @@ public class SortPairs {
     }
 }
 
-class CommitComparator implements Comparator<Pair> {
 
-    @Override
-    public int compare(Pair pair1, Pair pair2) {
-        int commit1 = pair1.getCommit();
-        int commit2 = pair2.getCommit();
-        return - (commit1 - commit2);
-    }
-}
