ae417795d66fe7b9c038826bcab4e74726ad165e
Max Ellis <mjellis@ualberta.ca>
2021-03-03 12:50:33 -0800

    Add testing for SortPairs

--- src/test/java/ca/ualberta/cs/smr/utils/sortUtils/SortUtilsTests.java
+++ src/test/java/ca/ualberta/cs/smr/utils/sortUtils/SortUtilsTests.java
@@ -1,9 +1,7 @@
 package ca.ualberta.cs.smr.utils.sortUtils;
 
 import ca.ualberta.cs.smr.testUtils.GetDataForTests;
-import ca.ualberta.cs.smr.utils.sortingUtils.CommitComparator;
-import ca.ualberta.cs.smr.utils.sortingUtils.Pair;
-import ca.ualberta.cs.smr.utils.sortingUtils.RefactoringComparator;
+import ca.ualberta.cs.smr.utils.sortingUtils.*;
 import org.junit.Assert;
 import org.junit.Test;
 import org.refactoringminer.api.Refactoring;
@@ -28,7 +26,7 @@ public class SortUtilsTests {
             pairsToSort.add(new Pair(i, null));
         }
         pairsToSort.sort(new CommitComparator());
-        Assert.assertTrue(isSorted(pairsToSort));
+        Assert.assertTrue(isCommitValueSorted(pairsToSort));
 
     }
 
@@ -47,6 +45,39 @@ public class SortUtilsTests {
         Assert.assertTrue(isRefactoringListSorted(typesToSort));
     }
 
+    @Test
+    public void testSortPairs() {
+        List<Pair> pairsToSort = new ArrayList<>();
+        for(int count = 0; count < 10; count++) {
+            Refactoring ref = GetDataForTests.getEmptyRefactoring(RefactoringType.RENAME_METHOD);
+            Pair pair = new Pair(count, ref);
+            pairsToSort.add(pair);
+            ref = GetDataForTests.getEmptyRefactoring(RefactoringType.RENAME_CLASS);
+            pair = new Pair(count, ref);
+            pairsToSort.add(pair);
+        }
+        SortPairs.sortList(pairsToSort);
+        Assert.assertTrue(isRefactoringAndCommitSorted(pairsToSort));
+    }
+
+    private boolean isRefactoringAndCommitSorted(List<Pair> list) {
+        for(int i = 0; i < list.size() - 1; i++) {
+            int commit1 = list.get(i).getCommit();
+            int commit2 = list.get(i+1).getCommit();
+            RefactoringType type1 = list.get(i).getValue().getRefactoringType();
+            RefactoringType type2 = list.get(i+1).getValue().getRefactoringType();
+            if(commit1 < commit2) {
+                return false;
+            }
+            else if(commit1 == commit2) {
+                if(refactoringTypeMap.get(type1) < refactoringTypeMap.get(type2)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     private boolean isRefactoringListSorted(List<Pair> list) {
         for(int i = 0; i < list.size() - 1; i++) {
             RefactoringType type1 = list.get(i).getValue().getRefactoringType();
@@ -57,7 +88,7 @@ public class SortUtilsTests {
         }
         return true;
     }
-    private boolean isSorted(List<Pair> list) {
+    private boolean isCommitValueSorted(List<Pair> list) {
         for(int i = 0; i < list.size() - 1; i++) {
             if(list.get(i).getCommit() < list.get(i+1).getCommit()) {
                 return false;
