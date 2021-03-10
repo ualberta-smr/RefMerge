f21ca41186747b8dbbae754907a345050ffe5a1f
Max Ellis <mjellis@ualberta.ca>
2021-03-10 15:33:25 -0800

    Remove edge weight and add visited boolean to node

--- src/main/java/ca/ualberta/cs/smr/core/dependenceGraph/Edge.java
+++ src/main/java/ca/ualberta/cs/smr/core/dependenceGraph/Edge.java
@@ -3,11 +3,9 @@ package ca.ualberta.cs.smr.core.dependenceGraph;
 public class Edge {
     Node node1;
     Node node2;
-    int weight;
 
-    Edge(Node node1, int weight, Node node2) {
+    Edge(Node node1, Node node2) {
         this.node1 = node1;
-        this.weight = weight;
         this.node2 = node2;
     }
 
@@ -19,7 +17,4 @@ public class Edge {
         return node2;
     }
 
-    public int getWeight() {
-        return weight;
-    }
 }
