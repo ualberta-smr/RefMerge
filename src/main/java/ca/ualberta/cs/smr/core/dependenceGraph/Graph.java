69285d933cea680a1e802ddf8c176f9dba1ebe7f
Max Ellis <mjellis@ualberta.ca>
2021-03-10 15:59:18 -0800

    Replace call to matrix in graph with dependence logic

--- src/main/java/ca/ualberta/cs/smr/core/dependenceGraph/Graph.java
+++ src/main/java/ca/ualberta/cs/smr/core/dependenceGraph/Graph.java
@@ -1,11 +1,11 @@
 package ca.ualberta.cs.smr.core.dependenceGraph;
 
-import ca.ualberta.cs.smr.core.matrix.Matrix;
-import ca.ualberta.cs.smr.core.matrix.elements.RefactoringElement;
-import ca.ualberta.cs.smr.core.matrix.visitors.RefactoringVisitor;
 import ca.ualberta.cs.smr.utils.sortingUtils.Pair;
 import com.intellij.openapi.project.Project;
+import gr.uom.java.xmi.diff.RenameClassRefactoring;
+import gr.uom.java.xmi.diff.RenameOperationRefactoring;
 import org.refactoringminer.api.Refactoring;
+import org.refactoringminer.api.RefactoringType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -78,15 +78,37 @@ public class Graph {
     public boolean hasDependence(Node node1, Node node2) {
         Refactoring refactoring1 = node1.getRefactoring();
         Refactoring refactoring2 = node2.getRefactoring();
-        if(refactoring1.getRefactoringType() == refactoring2.getRefactoringType()) {
-            return false;
+        RefactoringType type1 = refactoring1.getRefactoringType();
+        RefactoringType type2 = refactoring2.getRefactoringType();
+        if(type1 == RefactoringType.RENAME_CLASS && type2 == RefactoringType.RENAME_CLASS) {
+            // Only need to check this case because the node cannot depend on the new node
+            String class1 = ((RenameClassRefactoring) refactoring1).getRenamedClassName();
+            String class2 = ((RenameClassRefactoring) refactoring2).getOriginalClassName();
+            return class1.equals(class2);
         }
-        Matrix matrix = new Matrix(project);
-        RefactoringElement element = matrix.makeElement(refactoring1);
-        RefactoringVisitor visitor = matrix.makeVisitor(refactoring2);
-        element.accept(visitor);
+        else if(type1 == RefactoringType.RENAME_CLASS && type2 == RefactoringType.RENAME_METHOD) {
+            // Check if they happen in the same commit
+            String class1 = ((RenameClassRefactoring) refactoring1).getOriginalClassName();
+            String class2 = ((RenameOperationRefactoring) refactoring2).getOriginalOperation().getClassName();
+            if(class1.equals(class2)) {
+                return true;
+            }
+            // Check if the method is renamed in a commit after the class is renamed
+            class1 = ((RenameClassRefactoring) refactoring1).getRenamedClassName();
+            return class1.equals(class2);
+        }
+        else if(type1 == RefactoringType.RENAME_METHOD && type2 == RefactoringType.RENAME_CLASS) {
+            // Do the same thing as above
+            String class1 = ((RenameOperationRefactoring) refactoring1).getOriginalOperation().getClassName();
+            String class2 = ((RenameClassRefactoring) refactoring2).getOriginalClassName();
+            if(class1.equals(class2)) {
+                return true;
+            }
+            class2 = ((RenameClassRefactoring) refactoring2).getRenamedClassName();
+            return class1.equals(class2);
+        }
+        return false;
 
-        return visitor.getDependenceResult();
 
     }
 
