beed93d08f46e22f402d854a095dfb41ea46db14
Max Ellis <mjellis@ualberta.ca>
2021-03-11 20:55:52 -0800

    Add test for nested refactoring conflict

--- src/main/java/ca/ualberta/cs/smr/core/dependenceGraph/Edge.java
+++ src/main/java/ca/ualberta/cs/smr/core/dependenceGraph/Edge.java
@@ -4,7 +4,7 @@ public class Edge {
     Node node1;
     Node node2;
 
-    Edge(Node node1, Node node2) {
+    public Edge(Node node1, Node node2) {
         this.node1 = node1;
         this.node2 = node2;
     }
