3231a9e21b7993a278e7664165606f2a92780d12
Max Ellis <mjellis@ualberta.ca>
2021-03-22 15:48:11 -0700

    Revert extract method refactoring

---
+++ src/test/testData/extractTestData/extractMethod/refactored/Main.java
@@ -0,0 +1,29 @@
+
+
+public class Main {
+
+    public static boolean foo() {
+        method();
+        boolean bool = false;
+        for(int i = 0; i < 10; i++) {
+            if(bool) {
+                return true;
+            }
+        }
+        return bool;
+    }
+
+    public static void method() {
+        int x = 5;
+        int y = 10;
+        extractedMethod(x, y);
+    }
+
+    private void extractedMethod(int x, int y) {
+        System.out.println("Adding " + x + " and " + y);
+        int z = x + y;
+        String str = "Printing: ";
+        System.out.println(str + z);
+    }
+
+}
