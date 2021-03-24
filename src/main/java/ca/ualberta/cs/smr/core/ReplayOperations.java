8eb4c9273b61f3fbca5cfa6cba984ef0517237d9
Max Ellis <mjellis@ualberta.ca>
2021-03-24 01:08:04 -0700

    Replay extract method and test

--- src/main/java/ca/ualberta/cs/smr/core/ReplayOperations.java
+++ src/main/java/ca/ualberta/cs/smr/core/ReplayOperations.java
@@ -1,18 +1,29 @@
 package ca.ualberta.cs.smr.core;
 
 import ca.ualberta.cs.smr.utils.Utils;
+import com.intellij.lang.ASTNode;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.*;
+import com.intellij.psi.impl.source.SourceTreeToPsiMap;
 import com.intellij.refactoring.JavaRefactoringFactory;
 import com.intellij.refactoring.RefactoringFactory;
 import com.intellij.refactoring.RenameRefactoring;
+import com.intellij.refactoring.extractMethod.ExtractMethodHandler;
+import com.intellij.refactoring.extractMethod.ExtractMethodProcessor;
+import com.intellij.refactoring.extractMethod.PrepareFailedException;
 import com.intellij.usageView.UsageInfo;
 import gr.uom.java.xmi.UMLClass;
 import gr.uom.java.xmi.UMLOperation;
+import gr.uom.java.xmi.UMLParameter;
+import gr.uom.java.xmi.decomposition.AbstractCodeFragment;
+import gr.uom.java.xmi.diff.ExtractOperationRefactoring;
 import gr.uom.java.xmi.diff.RenameClassRefactoring;
 import gr.uom.java.xmi.diff.RenameOperationRefactoring;
 import org.refactoringminer.api.Refactoring;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 
 public class ReplayOperations {
@@ -68,4 +79,84 @@ public class ReplayOperations {
 
     }
 
+    public void replayExtractMethod(Refactoring ref) throws PrepareFailedException {
+
+        ExtractOperationRefactoring extractOperationRefactoring = (ExtractOperationRefactoring) ref;
+        UMLOperation sourceOperation = extractOperationRefactoring.getSourceOperationBeforeExtraction();
+        UMLOperation extractedOperation = extractOperationRefactoring.getExtractedOperation();
+        String refactoringName = extractedOperation.getName();
+        String initialMethodName = sourceOperation.getName();
+        String initialClassName = sourceOperation.getClassName();
+        String filePath = sourceOperation.getLocationInfo().getFilePath();
+        String helpId = "";
+
+        Utils utils = new Utils(project);
+        PsiClass psiClass = utils.getPsiClassFromClassAndFileNames(initialClassName, filePath);
+        assert psiClass != null;
+        PsiMethod psiMethod = Utils.getPsiMethod(psiClass, sourceOperation);
+        assert psiMethod != null;
+        PsiElement[] psiElements = getPsiElements(extractOperationRefactoring, psiMethod);
+        PsiType forcedReturnType = getPsiReturnType(extractOperationRefactoring, psiMethod);
+
+        ExtractMethodProcessor extractMethodProcessor = new ExtractMethodProcessor(project, null, psiElements,
+                forcedReturnType, refactoringName, initialMethodName, helpId);
+        extractMethodProcessor.setMethodName(refactoringName);
+        extractMethodProcessor.prepare();
+        extractMethodProcessor.setDataFromInputVariables();
+        ExtractMethodHandler.extractMethod(project, extractMethodProcessor);
+    }
+
+    private PsiElement[] getPsiElements(ExtractOperationRefactoring extractOperationRefactoring, PsiMethod psiMethod) {
+        ArrayList<PsiElement> psiElements = new ArrayList<>();
+        Set<AbstractCodeFragment> codeFragments = extractOperationRefactoring.getExtractedCodeFragmentsFromSourceOperation();
+        PsiCodeBlock psiCodeBlock = psiMethod.getBody();
+        assert psiCodeBlock != null;
+        for(AbstractCodeFragment codeFragment : codeFragments) {
+            int startOffset = codeFragment.getLocationInfo().getStartOffset();
+            int endOffset = codeFragment.getLocationInfo().getEndOffset();
+            ASTNode node = SourceTreeToPsiMap.psiElementToTree(psiMethod);
+            assert node != null;
+            ArrayList<ASTNode> nodes = getMethodNodes(node, startOffset, endOffset);
+            for(ASTNode astNode : nodes) {
+                psiElements.add(astNode.getPsi());
+            }
+        }
+        return psiElements.toArray(new PsiElement[0]);
+    }
+
+    private ArrayList<ASTNode> getMethodNodes(ASTNode node, int start, int end) {
+        ArrayList<ASTNode> astNodes = new ArrayList<>();
+        ASTNode[] children = node.getChildren(null);
+        if(children.length < 1) {
+            return astNodes;
+        }
+        int nodeStart = node.getStartOffset();
+        int nodeEnd = node.getTextRange().getEndOffset();
+        if (nodeStart >= start && nodeEnd <= end) {
+            PsiElement psiElement = node.getPsi();
+            if(!(psiElement instanceof  PsiJavaToken) && !(psiElement instanceof  PsiReferenceParameterList)
+                    && !(psiElement instanceof PsiReferenceExpression) && !(psiElement instanceof PsiBinaryExpression)
+                    && !(psiElement instanceof  PsiExpressionList) && !(psiElement instanceof PsiMethodCallExpression)) {
+                astNodes.add(node);
+            }
+        }
+
+        for(ASTNode child : children) {
+            astNodes.addAll(getMethodNodes(child, start, end));
+        }
+
+        return astNodes;
+    }
+
+    private PsiType getPsiReturnType(ExtractOperationRefactoring extractOperationRefactoring, PsiMethod psiMethod) {
+        UMLParameter returnParameter = extractOperationRefactoring.getExtractedOperation().getReturnParameter();
+        String parameterType = returnParameter.getType().toString();
+        PsiElementFactory factory = PsiElementFactory.getInstance(project);
+        if(parameterType.equals("void")) {
+            return factory.createTypeFromText(parameterType, psiMethod);
+        }
+        String parameterName = returnParameter.getName();
+        String parameter = parameterType + " " + parameterName;
+        return factory.createTypeFromText(parameter, psiMethod);
+    }
 }
