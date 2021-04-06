8d10eb6330b2a78a248ecc360689d0591d5095c1
Max Ellis <mjellis@ualberta.ca>
2021-04-06 12:33:33 -0700

    Update test receiver and element tests

--- src/test/java/ca/ualberta/cs/smr/core/matrix/receivers/RenameClassReceiverTest.java
+++ src/test/java/ca/ualberta/cs/smr/core/matrix/receivers/RenameClassReceiverTest.java
@@ -1,9 +1,12 @@
 package ca.ualberta.cs.smr.core.matrix.receivers;
 
 import ca.ualberta.cs.smr.core.dependenceGraph.Node;
+import ca.ualberta.cs.smr.core.matrix.logicCells.RenameClassRenameClassCell;
+import ca.ualberta.cs.smr.core.matrix.logicCells.RenameClassRenameMethodCell;
 import ca.ualberta.cs.smr.testUtils.GetDataForTests;
 import ca.ualberta.cs.smr.core.matrix.elements.RenameClassElement;
 import ca.ualberta.cs.smr.core.matrix.elements.RenameMethodElement;
+import com.intellij.openapi.project.Project;
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
 import org.junit.Assert;
 import org.refactoringminer.api.Refactoring;
@@ -46,4 +49,43 @@ public class RenameClassReceiverTest extends LightJavaCodeInsightFixtureTestCase
         verify(receiver, never()).receive(wrongElement);
     }
 
+    public void testRenameClassRenameClassConflictCell() {
+        Project project = myFixture.getProject();
+        String basePath = System.getProperty("user.dir");
+        String originalPath = basePath + "/src/test/testData/renameClassRenameClassFiles/renameClassNamingConflict/original";
+        String refactoredPath = basePath + "/src/test/testData/renameClassRenameClassFiles/renameClassNamingConflict/refactored";
+        List<Refactoring> refactorings = GetDataForTests.getRefactorings("RENAME_CLASS", originalPath, refactoredPath);
+        assert refactorings != null && refactorings.size() == 3;
+        Refactoring foo = refactorings.get(0);
+        Refactoring foo2 = refactorings.get(1);
+        Refactoring bar = refactorings.get(2);
+        RenameClassElement renameClassElement = new RenameClassElement();
+        Node elementNode = new Node(foo);
+        Node visitorNode = new Node(foo2);
+        renameClassElement.set(elementNode, project);
+        boolean isConflicting = RenameClassRenameClassCell.renameClassRenameClassConflictCell(elementNode, visitorNode);
+        Assert.assertTrue(isConflicting);
+        visitorNode = new Node(bar);
+        isConflicting = RenameClassRenameClassCell.renameClassRenameClassConflictCell(elementNode, visitorNode);
+        Assert.assertFalse(isConflicting);
+    }
+
+    public void testRenameClassRenameMethodDependenceCell() {
+        Project project = myFixture.getProject();
+        String basePath = System.getProperty("user.dir");
+        String originalPath = basePath + "/src/test/testData/renameMethodRenameClassFiles/dependence/original";
+        String refactoredPath = basePath + "/src/test/testData/renameMethodRenameClassFiles/dependence/refactored";
+        List<Refactoring> methodRefs = GetDataForTests.getRefactorings("RENAME_METHOD", originalPath, refactoredPath);
+        List<Refactoring> classRefs = GetDataForTests.getRefactorings("RENAME_CLASS", originalPath, refactoredPath);
+        assert methodRefs != null;
+        Refactoring visitorRef = methodRefs.get(0);
+        assert classRefs != null;
+        Refactoring elementRef = classRefs.get(0);
+        Node elementNode = new Node(elementRef);
+        Node visitorNode = new Node(visitorRef);
+        RenameClassElement element = new RenameClassElement();
+        element.set(elementNode, project);
+        boolean isDependent = RenameClassRenameMethodCell.renameClassRenameMethodDependenceCell(elementNode, visitorNode);
+        Assert.assertTrue(isDependent);
+    }
 }
