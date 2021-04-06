f93737dfa98b0231e127059fa41b354191e316c2
Max Ellis <mjellis@ualberta.ca>
2021-04-06 14:23:55 -0700

    Update DependenceGraphTests

--- src/test/java/ca/ualberta/cs/smr/core/dependenceGraph/DependenceGraphTests.java
+++ src/test/java/ca/ualberta/cs/smr/core/dependenceGraph/DependenceGraphTests.java
@@ -19,31 +19,6 @@ public class DependenceGraphTests extends LightJavaCodeInsightFixtureTestCase {
         return "src/test/testData";
     }
 
-    public void testHasDependence() {
-        String basePath = System.getProperty("user.dir");
-        String originalPath = basePath + "/src/test/testData/renameMethodRenameClassFiles/dependenceGraph/original";
-        String refactoredPath = basePath + "/src/test/testData/renameMethodRenameClassFiles/dependenceGraph/refactored";
-
-        Project project = myFixture.getProject();
-        List<Refactoring> methodRefs = GetDataForTests.getRefactorings("RENAME_METHOD", originalPath, refactoredPath);
-        List<Refactoring> classRefs = GetDataForTests.getRefactorings("RENAME_CLASS", originalPath, refactoredPath);
-
-        assert classRefs != null;
-        assert methodRefs != null;
-        Graph graph = new Graph(project);
-        Node methodNode = new Node(methodRefs.get(0));
-        Node classNode = new Node(classRefs.get(0));
-        boolean hasDependence = graph.hasDependence(methodNode, classNode);
-        Assert.assertTrue(hasDependence);
-        Node methodNode2 = new Node(methodRefs.get(1));
-        hasDependence = graph.hasDependence(methodNode2, classNode);
-        Assert.assertTrue(hasDependence);
-        hasDependence = graph.hasDependence(methodNode, methodNode2);
-        Assert.assertFalse(hasDependence);
-
-
-    }
-
 
     public void testDependenceGraph() {
         String testDir = "renameMethodRenameClassFiles/dependenceGraph/";
@@ -68,37 +43,35 @@ public class DependenceGraphTests extends LightJavaCodeInsightFixtureTestCase {
 
         assert classRefs != null;
         assert methodRefs != null;
-        leftList.add(new Pair(0, classRefs.get(0))); // Original -> A
-        leftList.add(new Pair(1, methodRefs.get(0))); // A.foo -> A.bar
-        rightList.add(new Pair(0, methodRefs.get(1))); //Original.foo -> Original.fuzz
+        leftList.add(new Pair(0, classRefs.get(1))); // Original -> A
+        leftList.add(new Pair(1, methodRefs.get(2))); // A.foo -> A.bar
+        rightList.add(new Pair(0, methodRefs.get(3))); //Original.foo -> Original.fuzz
 
-        System.out.println("Left List:");
+        List<Refactoring> expectedList = new ArrayList<>();
+        expectedList.add(methodRefs.get(3));
+        expectedList.add(classRefs.get(1));
+        expectedList.add(methodRefs.get(2));
+
         SortPairs.sortList(leftList);
-        for( Pair pair : leftList) {
-            System.out.println(pair.getValue().toString());
-        }
-        System.out.println("Right List");
         SortPairs.sortList(rightList);
-        for(Pair pair : rightList) {
-            System.out.println(pair.getValue().toString());
-        }
-        Graph graph = new Graph(project);
+
+        DependenceGraph graph = new DependenceGraph(project);
         graph.createPartialGraph(leftList);
         graph.createPartialGraph(rightList);
-        System.out.println("\n\nGRAPH");
-        graph.printGraph();
-        System.out.println("\n\n");
+        List<Node> originalNodes = graph.getSortedNodes();
         Matrix matrix = new Matrix(project);
-        Graph newGraph = matrix.runMatrix(leftList, rightList);
+        DependenceGraph newGraph = matrix.runMatrix(leftList, rightList);
         System.out.println("\n\nNEW GRAPH:");
-        List<Node> nodes = newGraph.getSortedNodes();
-        for(Node node : nodes) {
-            if(node.wasVisited()) {
-                System.out.println("VISITED: " + node.getRefactoring().toString());
-            }
-            else {
-                System.out.println("NOT VISITED");
-            }
+        List<Node> sortedNodes = newGraph.getSortedNodes();
+
+        List<Refactoring> unexpectedList = new ArrayList<>();
+        List<Refactoring> actualList = new ArrayList<>();
+        for(int i = 0; i < originalNodes.size(); i++) {
+            unexpectedList.add(originalNodes.get(i).getRefactoring());
+            actualList.add(sortedNodes.get(i).getRefactoring());
         }
+        Assert.assertNotEquals(unexpectedList, actualList);
+        Assert.assertEquals(expectedList, actualList);
     }
+
 }
