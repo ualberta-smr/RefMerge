1d0d9f0b32ecc28241979ae87491f87ef02463c1
Max Ellis <mjellis@ualberta.ca>
2021-06-07 16:33:10 -0700

    Add rename class/rename method combination logic and update if a combination is found

--- src/main/java/ca/ualberta/cs/smr/core/matrix/logicCells/RenameClassRenameMethodCell.java
+++ src/main/java/ca/ualberta/cs/smr/core/matrix/logicCells/RenameClassRenameMethodCell.java
@@ -1,6 +1,9 @@
 package ca.ualberta.cs.smr.core.matrix.logicCells;
 
 import ca.ualberta.cs.smr.core.dependenceGraph.Node;
+import ca.ualberta.cs.smr.core.refactoringObjects.RefactoringObject;
+import ca.ualberta.cs.smr.core.refactoringObjects.RenameClassObject;
+import ca.ualberta.cs.smr.core.refactoringObjects.RenameMethodObject;
 import gr.uom.java.xmi.diff.RenameClassRefactoring;
 import gr.uom.java.xmi.diff.RenameOperationRefactoring;
 import org.refactoringminer.api.Refactoring;
@@ -25,5 +28,35 @@ public class RenameClassRenameMethodCell {
         String classClass = ((RenameClassRefactoring) classRefactoring).getOriginalClass().getName();
         String methodClass = ((RenameOperationRefactoring) methodRefactoring).getOriginalOperation().getClassName();
         return classClass.equals(methodClass);
+    }
+
+    /*
+     * Check if the rename class and rename method refactorings can be simplified. If they can, update the rename method
+     * refactoring class details.
+     */
+    public static void checkRenameClassRenameMethodCombination(RefactoringObject renameMethod,
+                                                               RefactoringObject renameClass) {
+        RenameMethodObject methodObject = (RenameMethodObject) renameMethod;
+        String originalMethodClass = methodObject.getOriginalClassName();
+        String destinationMethodClass = methodObject.getDestinationClassName();
+        RenameClassObject classObject = (RenameClassObject) renameClass;
+        String originalClassClass = classObject.getOriginalClassName();
+        String destinationClassClass = classObject.getDestinationClassName();
+
+        // If the original class of the rename method refactoring is the same as the original class of the rename class
+        // refactoring, update the destination class for the rename method refactoring to be the destination class of
+        // the rename class refactoring
+        if(originalMethodClass.equals(originalClassClass) && !destinationMethodClass.equals(destinationClassClass)) {
+            renameMethod.setDestinationFilePath(classObject.getDestinationFilePath());
+            ((RenameMethodObject) renameMethod).setDestinationClassName(classObject.getDestinationClassName());
+        }
+        // If the destination classes for the rename method and rename class refactorings are the same but the original
+        // names are different, then the class was renamed before the method was and we need to update the original
+        // class name for the rename method refactoring
+        else if(!originalMethodClass.equals(originalClassClass) && destinationMethodClass.equals(destinationClassClass)) {
+            renameMethod.setOriginalFilePath(classObject.getOriginalFilePath());
+            ((RenameMethodObject) renameMethod).setOriginalClassName(classObject.getOriginalClassName());
+        }
+
     }
 }
