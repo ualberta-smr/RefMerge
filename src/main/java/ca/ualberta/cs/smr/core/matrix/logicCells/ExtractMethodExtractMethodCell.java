fbefa6b919c1ae3592736f27defa41fc1693d963
Max Ellis <mjellis@ualberta.ca>
2021-06-09 21:14:38 -0700

    Update extract method/extract method conflict logic and tests to use simplified refactorings

--- src/main/java/ca/ualberta/cs/smr/core/matrix/logicCells/ExtractMethodExtractMethodCell.java
+++ src/main/java/ca/ualberta/cs/smr/core/matrix/logicCells/ExtractMethodExtractMethodCell.java
@@ -1,13 +1,12 @@
 package ca.ualberta.cs.smr.core.matrix.logicCells;
 
-import ca.ualberta.cs.smr.core.dependenceGraph.Node;
+import ca.ualberta.cs.smr.core.refactoringObjects.ExtractMethodObject;
+import ca.ualberta.cs.smr.core.refactoringObjects.MethodSignatureObject;
+import ca.ualberta.cs.smr.core.refactoringObjects.RefactoringObject;
 import ca.ualberta.cs.smr.utils.Utils;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiClass;
-import gr.uom.java.xmi.UMLOperation;
 import gr.uom.java.xmi.decomposition.AbstractCodeFragment;
-import gr.uom.java.xmi.diff.ExtractOperationRefactoring;
-import org.refactoringminer.api.Refactoring;
 
 import java.util.Set;
 
@@ -25,24 +24,22 @@ public class ExtractMethodExtractMethodCell {
     /*
      *  Check if a refactoring conflict exists between extract method/rename method refactorings. A refactoring conflict
      *  can occur if there is an overlapping fragments, accidental override, accidental overload, or naming conflict.
-     *  @param dispatcherNode: A node containing the dispatcher extract method refactoring.
-     *  @param receiverNode: A node containing the receiver extract method refactoring.
      */
-    public boolean extractMethodExtractMethodConflictCell(Node dispatcherNode, Node receiverNode) {
+    public boolean extractMethodExtractMethodConflictCell(RefactoringObject dispatcherObject, RefactoringObject receiverObject) {
         // Extract Method/Extract Method overlapping fragments conflict
-        if(checkOverlappingFragmentsConflict(dispatcherNode, receiverNode)) {
+        if(checkOverlappingFragmentsConflict(dispatcherObject, receiverObject)) {
             return true;
         }
         // Extract Method/Extract Method accidental override conflict
-        else if(checkOverrideConflict(dispatcherNode, receiverNode)) {
+        else if(checkOverrideConflict(dispatcherObject, receiverObject)) {
             return true;
         }
         // Extract Method/Extract Method accidental override conflict
-        else if(checkOverloadConflict(dispatcherNode, receiverNode)) {
+        else if(checkOverloadConflict(dispatcherObject, receiverObject)) {
             return true;
         }
         // Extract Method/Extract Method naming conflict
-        return checkMethodNamingConflict(dispatcherNode, receiverNode);
+        return checkMethodNamingConflict(dispatcherObject, receiverObject);
     }
 
     /*
@@ -50,21 +47,25 @@ public class ExtractMethodExtractMethodCell {
      * and the extracted fragments overlap. The extracted fragments overlap if a code fragment in the source method is
      * extracted to both extracted methods.
      */
-    public boolean checkOverlappingFragmentsConflict(Node dispatcherNode, Node receiverNode) {
-        ExtractOperationRefactoring dispatcherRefactoring = (ExtractOperationRefactoring) dispatcherNode.getRefactoring();
-        ExtractOperationRefactoring receiverRefactoring = (ExtractOperationRefactoring) receiverNode.getRefactoring();
+    public boolean checkOverlappingFragmentsConflict(RefactoringObject dispatcherObject, RefactoringObject receiverObject) {
+        ExtractMethodObject dispatcherExtractMethod = (ExtractMethodObject) dispatcherObject;
+        ExtractMethodObject receiverExtractMethod = (ExtractMethodObject) receiverObject;
 
         // Get the refactored operations
-        UMLOperation sourceDispatcher = dispatcherRefactoring.getSourceOperationBeforeExtraction();
-        UMLOperation sourceReceiver = receiverRefactoring.getSourceOperationBeforeExtraction();
+        MethodSignatureObject dispatcherOriginalMethod = dispatcherExtractMethod.getOriginalMethodSignature();
+        MethodSignatureObject receiverOriginalMethod = receiverExtractMethod.getOriginalMethodSignature();
+
+        String dispatcherOriginalClassName = dispatcherExtractMethod.getOriginalClassName();
+        String receiverOriginalClassName = receiverExtractMethod.getOriginalClassName();
 
         // If the source methods are in different classes or are not the same method, return false
-        if(!sourceDispatcher.equalsQualified(sourceReceiver)) {
+        if(!dispatcherOriginalClassName.equals(receiverOriginalClassName)
+                && !dispatcherOriginalMethod.equalsSignature(receiverOriginalMethod)) {
             return false;
         }
 
-        Set<AbstractCodeFragment> dispatcherFragments = dispatcherRefactoring.getExtractedCodeFragmentsFromSourceOperation();
-        Set<AbstractCodeFragment> receiverFragments = receiverRefactoring.getExtractedCodeFragmentsFromSourceOperation();
+        Set<AbstractCodeFragment> dispatcherFragments = dispatcherExtractMethod.getExtractedCodeFragments();
+        Set<AbstractCodeFragment> receiverFragments = receiverExtractMethod.getExtractedCodeFragments();
         // Check if a code fragment exists in both extracted methods
         for(AbstractCodeFragment dispatcherFragment : dispatcherFragments) {
             for(AbstractCodeFragment receiverFragment : receiverFragments) {
@@ -82,76 +83,62 @@ public class ExtractMethodExtractMethodCell {
      * An override conflict will occur between two extract method refactorings if the extracted methods have the same signature
      * and their classes are in an inheritance relationship.
      */
-    public boolean checkOverrideConflict(Node dispatcherNode, Node receiverNode) {
-        Refactoring dispatcherRefactoring = dispatcherNode.getRefactoring();
-        Refactoring receiverRefactoring = receiverNode.getRefactoring();
+    public boolean checkOverrideConflict(RefactoringObject dispatcherObject, RefactoringObject receiverObject) {
+        ExtractMethodObject dispatcherExtractMethod = (ExtractMethodObject) dispatcherObject;
+        ExtractMethodObject receiverExtractMethod = (ExtractMethodObject) receiverObject;
 
         // Get the refactored operations
-        UMLOperation dispatcherOperation = ((ExtractOperationRefactoring) dispatcherRefactoring).getExtractedOperation();
-        UMLOperation receiverOperation = ((ExtractOperationRefactoring) receiverRefactoring).getExtractedOperation();
+        MethodSignatureObject dispatcherDestinationMethod = dispatcherExtractMethod.getDestinationMethodSignature();
+        MethodSignatureObject receiverDestinationMethod = receiverExtractMethod.getDestinationMethodSignature();
         // Get the class names
-        String dispatcherClassName = dispatcherNode.getDependenceChainClassHead();
-        String receiverClassName = receiverNode.getDependenceChainClassHead();
+        String dispatcherOriginalClassName = dispatcherExtractMethod.getOriginalClassName();
+        String receiverOriginalClassName = receiverExtractMethod.getOriginalClassName();
 
         // If the extract methods happen in the same class then there is no override conflict
-        if(isSameName(dispatcherClassName, receiverClassName)) {
-            if(isSameOriginalClass(dispatcherNode, receiverNode)) {
-                return false;
-            }
+        if(dispatcherOriginalClassName.equals(receiverOriginalClassName)) {
+            return false;
         }
+        String dispatcherOriginalFile = dispatcherExtractMethod.getOriginalFilePath();
+        String receiverOriginalFile = receiverExtractMethod.getOriginalFilePath();
         Utils utils = new Utils(project);
-        String dispatcherFile = dispatcherOperation.getLocationInfo().getFilePath();
-        String receiverFile = receiverOperation.getLocationInfo().getFilePath();
-        PsiClass dispatcherPsiClass = utils.getPsiClassByFilePath(dispatcherFile, dispatcherClassName);
-        PsiClass receiverPsiClass = utils.getPsiClassByFilePath(receiverFile, receiverClassName);
+        PsiClass dispatcherPsiClass = utils.getPsiClassByFilePath(dispatcherOriginalFile, dispatcherOriginalClassName);
+        PsiClass receiverPsiClass = utils.getPsiClassByFilePath(receiverOriginalFile, receiverOriginalClassName);
         if(!ifClassExtends(dispatcherPsiClass, receiverPsiClass)) {
             return false;
         }
 
-        if(!dispatcherOperation.equalSignature(receiverOperation)) {
-            return false;
-        }
-
-        String dispatcherMethodName = dispatcherOperation.getName();
-        String receiverMethodName = receiverOperation.getName();
-
-        // If the signatures are different, then it cannot be an accidental override
-        if(!dispatcherOperation.equalSignature(receiverOperation)) {
-            return false;
-        }
-        // If the signatures are the same and the names are the same then it is a case of accidental overriding
-        return dispatcherMethodName.equals(receiverMethodName);
+        return dispatcherDestinationMethod.equalsSignature(receiverDestinationMethod);
     }
 
     /*
      * An overload conflict will occur between two extract method refactorings if the extracted methods have the same name and
      * different signatures. They need to either be in the same class, or in two classes that have an inheritance relationship.
      */
-    public boolean checkOverloadConflict(Node dispatcherNode, Node receiverNode) {
-        Refactoring dispatcherRefactoring = dispatcherNode.getRefactoring();
-        Refactoring receiverRefactoring = receiverNode.getRefactoring();
+    public boolean checkOverloadConflict(RefactoringObject dispatcherObject, RefactoringObject receiverObject) {
+        ExtractMethodObject dispatcherExtractMethod = (ExtractMethodObject) dispatcherObject;
+        ExtractMethodObject receiverExtractMethod = (ExtractMethodObject) receiverObject;
 
-        UMLOperation dispatcherOperation = ((ExtractOperationRefactoring) dispatcherRefactoring).getExtractedOperation();
-        UMLOperation receiverOperation = ((ExtractOperationRefactoring) receiverRefactoring).getExtractedOperation();
+        MethodSignatureObject dispatcherDestinationMethod = dispatcherExtractMethod.getDestinationMethodSignature();
+        MethodSignatureObject receiverDestinationMethod = receiverExtractMethod.getDestinationMethodSignature();
         // Get class names
-        String dispatcherClassName = dispatcherNode.getDependenceChainClassHead();
-        String receiverClassName = receiverNode.getDependenceChainClassHead();
+        String dispatcherOriginalClassName = dispatcherExtractMethod.getOriginalClassName();
+        String receiverOriginalClassName = receiverExtractMethod.getOriginalClassName();
         // If the methods are in different classes, check if one class inherits the other
-        if (!isSameName(dispatcherClassName, receiverClassName)) {
+        if (!dispatcherOriginalClassName.equals(receiverOriginalClassName)) {
             Utils utils = new Utils(project);
-            String dispatcherFile = dispatcherOperation.getLocationInfo().getFilePath();
-            String receiverFile = receiverOperation.getLocationInfo().getFilePath();
-            PsiClass dispatcherPsiClass = utils.getPsiClassByFilePath(dispatcherFile, dispatcherClassName);
-            PsiClass receiverPsiClass = utils.getPsiClassByFilePath(receiverFile, receiverClassName);
+            String dispatcherOriginalFile = dispatcherExtractMethod.getOriginalFilePath();
+            String receiverOriginalFile = receiverExtractMethod.getOriginalFilePath();
+            PsiClass dispatcherPsiClass = utils.getPsiClassByFilePath(dispatcherOriginalFile, dispatcherOriginalClassName);
+            PsiClass receiverPsiClass = utils.getPsiClassByFilePath(receiverOriginalFile, receiverOriginalClassName);
             if(!ifClassExtends(dispatcherPsiClass, receiverPsiClass)) {
                 return false;
             }
         }
-        String dispatcherMethodName = dispatcherOperation.getName();
-        String receiverMethodName = receiverOperation.getName();
+        String dispatcherMethodName = dispatcherDestinationMethod.getName();
+        String receiverMethodName = receiverDestinationMethod.getName();
 
         // If the signatures are equal, then it is not an accidental overload
-        if(dispatcherOperation.equalSignature(receiverOperation)) {
+        if(dispatcherDestinationMethod.equalsSignature(receiverDestinationMethod)) {
             return false;
         }
         // If the signatures are different and the names are the same, it is an accidental overload
@@ -162,27 +149,21 @@ public class ExtractMethodExtractMethodCell {
      * A naming conflict will occur between two extract method refactorings if the extracted methods exist in the same class
      * and they have the same signature.
      */
-    public boolean checkMethodNamingConflict(Node dispatcherNode, Node receiverNode) {
-        Refactoring dispatcherRefactoring = dispatcherNode.getRefactoring();
-        Refactoring receiverRefactoring = receiverNode.getRefactoring();
-        UMLOperation dispatcherOperation = ((ExtractOperationRefactoring) dispatcherRefactoring).getExtractedOperation();
-        UMLOperation receiverOperation = ((ExtractOperationRefactoring) receiverRefactoring).getExtractedOperation();
-        String dispatcherClassName = dispatcherOperation.getClassName();
-        String receiverClassName = receiverOperation.getClassName();
+    public boolean checkMethodNamingConflict(RefactoringObject dispatcherObject, RefactoringObject receiverObject) {
+        ExtractMethodObject dispatcherExtractMethod = (ExtractMethodObject) dispatcherObject;
+        ExtractMethodObject receiverExtractMethod = (ExtractMethodObject) receiverObject;
+
+        MethodSignatureObject dispatcherDestinationMethod = dispatcherExtractMethod.getDestinationMethodSignature();
+        MethodSignatureObject receiverDestinationMethod = receiverExtractMethod.getDestinationMethodSignature();
+
+        String dispatcherOriginalClassName = dispatcherExtractMethod.getOriginalClassName();
+        String receiverOriginalClassName = receiverExtractMethod.getOriginalClassName();
 
         // If the methods are in different classes
-        if (!isSameName(dispatcherClassName, receiverClassName)) {
-            if(!isSameOriginalClass(dispatcherNode, receiverNode))
-                return false;
-        }
-
-        if(!dispatcherOperation.equalSignature(receiverOperation)) {
+        if (!dispatcherOriginalClassName.equals(receiverOriginalClassName)) {
             return false;
         }
-        String dispatcherMethodName = dispatcherOperation.getName();
-        String receiverMethodName = receiverOperation.getName();
-        // Check if the extracted method names are the same
-        return dispatcherMethodName.equals(receiverMethodName);
+        return dispatcherDestinationMethod.equalsSignature(receiverDestinationMethod);
     }
 
 
