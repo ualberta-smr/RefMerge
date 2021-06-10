b6a3d823dd1840c96bf98ed9b72e347c1e1d5601
Max Ellis <mjellis@ualberta.ca>
2021-06-09 19:04:04 -0700

    Update rename method/rename method conflict logic and testing to use simplified refactoring objects

--- src/test/java/ca/ualberta/cs/smr/core/matrix/logicCells/RenameMethodRenameMethodLogicTests.java
+++ src/test/java/ca/ualberta/cs/smr/core/matrix/logicCells/RenameMethodRenameMethodLogicTests.java
@@ -1,11 +1,12 @@
 package ca.ualberta.cs.smr.core.matrix.logicCells;
 
-import ca.ualberta.cs.smr.core.dependenceGraph.Node;
+import ca.ualberta.cs.smr.core.matrix.Matrix;
 import ca.ualberta.cs.smr.core.refactoringObjects.MethodSignatureObject;
 import ca.ualberta.cs.smr.core.refactoringObjects.ParameterObject;
 import ca.ualberta.cs.smr.core.refactoringObjects.RefactoringObject;
 import ca.ualberta.cs.smr.core.refactoringObjects.RenameMethodObject;
 import ca.ualberta.cs.smr.testUtils.GetDataForTests;
+import ca.ualberta.cs.smr.utils.RefactoringObjectUtils;
 import com.intellij.openapi.project.Project;
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
 import org.junit.Assert;
@@ -36,14 +37,19 @@ public class RenameMethodRenameMethodLogicTests extends LightJavaCodeInsightFixt
         Refactoring renameChildBarMethod = refactorings.get(2);
         Refactoring renameOtherBarMethod = refactorings.get(3);
         Refactoring renameFooBarMethod = refactorings.get(4);
+        RefactoringObject leftRefactoringObject = RefactoringObjectUtils.createRefactoringObject(renameParentFooMethod);
+        RefactoringObject rightRefactoringObject = RefactoringObjectUtils.createRefactoringObject(renameOtherFooMethod);
         RenameMethodRenameMethodCell renameMethodRenameMethodCell = new RenameMethodRenameMethodCell(project);
-        boolean isConflicting = renameMethodRenameMethodCell.checkOverrideConflict(new Node(renameParentFooMethod), new Node(renameOtherFooMethod));
+        boolean isConflicting = renameMethodRenameMethodCell.checkOverrideConflict(leftRefactoringObject, rightRefactoringObject);
         Assert.assertFalse("Renamings in the same class should not result in override conflict", isConflicting);
-        isConflicting = renameMethodRenameMethodCell.checkOverrideConflict(new Node(renameParentFooMethod), new Node(renameOtherBarMethod));
+        rightRefactoringObject = RefactoringObjectUtils.createRefactoringObject(renameOtherBarMethod);
+        isConflicting = renameMethodRenameMethodCell.checkOverrideConflict(leftRefactoringObject, rightRefactoringObject);
         Assert.assertFalse("Methods that have no override relation, before or after, should not conflict", isConflicting);
-        isConflicting = renameMethodRenameMethodCell.checkOverrideConflict(new Node(renameParentFooMethod), new Node(renameFooBarMethod));
+        rightRefactoringObject = RefactoringObjectUtils.createRefactoringObject(renameFooBarMethod);
+        isConflicting = renameMethodRenameMethodCell.checkOverrideConflict(leftRefactoringObject, rightRefactoringObject);
         Assert.assertFalse("Classes that have no inheritance should not result in override conflicts", isConflicting);
-        isConflicting = renameMethodRenameMethodCell.checkOverrideConflict(new Node(renameParentFooMethod), new Node(renameChildBarMethod));
+        rightRefactoringObject = RefactoringObjectUtils.createRefactoringObject(renameChildBarMethod);
+        isConflicting = renameMethodRenameMethodCell.checkOverrideConflict(leftRefactoringObject, rightRefactoringObject);
         Assert.assertTrue("Originally overriding methods that are renamed to different names conflict", isConflicting);
     }
 
@@ -59,11 +65,34 @@ public class RenameMethodRenameMethodLogicTests extends LightJavaCodeInsightFixt
         assert refactorings.size() == 3;
         Refactoring leftRefactoring = refactorings.get(0);
         Refactoring rightRefactoring = refactorings.get(2);
+        RefactoringObject leftRefactoringObject = RefactoringObjectUtils.createRefactoringObject(leftRefactoring);
+        RefactoringObject rightRefactoringObject = RefactoringObjectUtils.createRefactoringObject(rightRefactoring);
         RenameMethodRenameMethodCell renameMethodRenameMethodCell = new RenameMethodRenameMethodCell(project);
-        boolean isConflicting = renameMethodRenameMethodCell.checkOverloadConflict(new Node(leftRefactoring), new Node(rightRefactoring));
+        boolean isConflicting = renameMethodRenameMethodCell.checkOverloadConflict(leftRefactoringObject, rightRefactoringObject);
         Assert.assertFalse("Methods in the same class that do not have related names " +
                 "before or after being refactored should not conflict", isConflicting);
+
+        List<ParameterObject> leftParameters = new ArrayList<>();
+        leftParameters.add(new ParameterObject("int", "return"));
+        MethodSignatureObject foo = new MethodSignatureObject(leftParameters, "foo");
+        MethodSignatureObject bar = new MethodSignatureObject(leftParameters, "bar");
+        List<ParameterObject> rightParameters = new ArrayList<>();
+        rightParameters.add(new ParameterObject("int", "return"));
+        rightParameters.add(new ParameterObject("int", "x"));
+        MethodSignatureObject foobar = new MethodSignatureObject(rightParameters, "foobar");
+        MethodSignatureObject bar2 = new MethodSignatureObject(rightParameters, "bar");
+        // A.foo -> A.bar
+        leftRefactoringObject = new RenameMethodObject("A.java", "A",
+                foo, "A.java", "A", bar);
+        // A.foobar(x) -> A.bar(x)
+        rightRefactoringObject = new RenameMethodObject("A.java", "A",
+                foobar, "A.java", "A", bar2);
+        isConflicting = renameMethodRenameMethodCell.checkOverloadConflict(leftRefactoringObject, rightRefactoringObject);
+        Assert.assertTrue("Methods in the same class that are renamed to the same name " +
+                "with different signatures should be an accidental overload", isConflicting);
     }
+
+
 
     public void testCheckRenameMethodRenameMethodNamingConflict() {
         Project project = myFixture.getProject();
@@ -72,20 +101,20 @@ public class RenameMethodRenameMethodLogicTests extends LightJavaCodeInsightFixt
         String refactoredPath = basePath + "/src/test/resources/renameMethodRenameMethodFiles/methodNamingConflict/refactored";
         List<Refactoring> refactorings = GetDataForTests.getRefactorings("RENAME_METHOD", originalPath, refactoredPath);
         assert refactorings != null;
-        Refactoring leftRef = refactorings.get(1);
-        Refactoring rightRef = refactorings.get(2);
-        Node leftNode = new Node(leftRef);
-        Node rightNode = new Node(rightRef);
+        Refactoring leftRefactoring = refactorings.get(1);
+        Refactoring rightRefactoring = refactorings.get(2);
+        RefactoringObject leftRefactoringObject = RefactoringObjectUtils.createRefactoringObject(leftRefactoring);
+        RefactoringObject rightRefactoringObject = RefactoringObjectUtils.createRefactoringObject(rightRefactoring);
         RenameMethodRenameMethodCell renameMethodRenameMethodCell = new RenameMethodRenameMethodCell(project);
-        boolean expectedFalse = renameMethodRenameMethodCell.checkMethodNamingConflict(leftNode, rightNode);
+        boolean expectedFalse = renameMethodRenameMethodCell.checkMethodNamingConflict(leftRefactoringObject, rightRefactoringObject);
         Assert.assertFalse("Methods in different classes should not have naming conflicts", expectedFalse);
-        rightRef = refactorings.get(0);
-        rightNode = new Node(rightRef);
-        boolean expectedTrue = renameMethodRenameMethodCell.checkMethodNamingConflict(leftNode, rightNode);
+        rightRefactoring = refactorings.get(0);
+        rightRefactoringObject = RefactoringObjectUtils.createRefactoringObject(rightRefactoring);
+        boolean expectedTrue = renameMethodRenameMethodCell.checkMethodNamingConflict(leftRefactoringObject, rightRefactoringObject);
         Assert.assertTrue("Methods renamed to the same name in the same class should return true", expectedTrue);
-        expectedTrue = renameMethodRenameMethodCell.checkMethodNamingConflict(rightNode, leftNode);
+        expectedTrue = renameMethodRenameMethodCell.checkMethodNamingConflict(rightRefactoringObject, leftRefactoringObject);
         Assert.assertTrue("The same refactorings in a different order should return true", expectedTrue);
-        expectedFalse = renameMethodRenameMethodCell.checkMethodNamingConflict(rightNode, rightNode);
+        expectedFalse = renameMethodRenameMethodCell.checkMethodNamingConflict(rightRefactoringObject, rightRefactoringObject);
         Assert.assertFalse("A method renamed to the same name in both versions should not conflict", expectedFalse);
     }
 
@@ -101,14 +130,17 @@ public class RenameMethodRenameMethodLogicTests extends LightJavaCodeInsightFixt
         Refactoring dispatcherRef = methodRefactorings.get(0);
         Refactoring rightRef = methodRefactorings.get(3);
         Refactoring classRef = classRefactorings.get(0);
-        Node dispatcherNode = new Node(dispatcherRef);
-        Node rightNode = new Node(rightRef);
-        Node classNode = new Node(classRef);
-        ArrayList<Node> nodes = new ArrayList<>();
-        nodes.add(classNode);
-        rightNode.addDependsList(nodes);
+
+        ArrayList<RefactoringObject> rightList = new ArrayList<>();
+        RefactoringObject rightRefactoringObject = RefactoringObjectUtils.createRefactoringObject(rightRef);
+        RefactoringObject rightClassRenameObject = RefactoringObjectUtils.createRefactoringObject(classRef);
+        RefactoringObject leftRefactoringObject = RefactoringObjectUtils.createRefactoringObject(dispatcherRef);
+        Matrix matrix = new Matrix(null);
+        matrix.simplifyAndInsertRefactorings(rightClassRenameObject,rightList);
+        matrix.simplifyAndInsertRefactorings(rightRefactoringObject,rightList);
+        rightRefactoringObject = rightList.get(1);
         RenameMethodRenameMethodCell renameMethodRenameMethodCell = new RenameMethodRenameMethodCell(project);
-        boolean isConflicting = renameMethodRenameMethodCell.checkMethodNamingConflict(dispatcherNode, rightNode);
+        boolean isConflicting = renameMethodRenameMethodCell.checkMethodNamingConflict(leftRefactoringObject, rightRefactoringObject);
         Assert.assertTrue(isConflicting);
     }
 
