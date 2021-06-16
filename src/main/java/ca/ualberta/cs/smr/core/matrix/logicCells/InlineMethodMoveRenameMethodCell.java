6c5201dd030f3197b8d301c6e6c3c891bae1a975
Max Ellis <mjellis@ualberta.ca>
2021-06-16 16:37:17 -0700

    Add inline method/move+rename method dependence cell

--- src/main/java/ca/ualberta/cs/smr/core/matrix/logicCells/InlineMethodMoveRenameMethodCell.java
+++ src/main/java/ca/ualberta/cs/smr/core/matrix/logicCells/InlineMethodMoveRenameMethodCell.java
@@ -23,7 +23,26 @@ public class InlineMethodMoveRenameMethodCell {
         if(!moveRenameOriginalMethod.equalsSignature(inlineOriginalMethod) || !moveRenameOriginalClass.equals(inlineOriginalClass)) {
             return false;
         }
-
         return true;
     }
+
+    /*
+     * Check if inline method and move+rename method refactorings have dependence between branches.
+     */
+    public static boolean inlineMethodMoveRenameMethodDependenceCell(RefactoringObject moveRenameMethod, RefactoringObject inlineMethod) {
+        MoveRenameMethodObject moveRenameMethodObject = (MoveRenameMethodObject) moveRenameMethod;
+        InlineMethodObject inlineMethodObject = (InlineMethodObject) inlineMethod;
+
+        MethodSignatureObject moveRenameOriginalMethod = moveRenameMethodObject.getOriginalMethodSignature();
+        MethodSignatureObject inlineDestinationMethod = ((InlineMethodObject) inlineMethod).getDestinationMethodSignature();
+        String moveRenameOriginalClass = moveRenameMethodObject.getOriginalClassName();
+        String inlineDestinationClass = inlineMethodObject.getDestinationClassName();
+
+        // If the target method in the inline method refactoring was moved/renamed, update the target method to the new method
+        if(!moveRenameOriginalMethod.equalsSignature(inlineDestinationMethod) || !moveRenameOriginalClass.equals(inlineDestinationClass)) {
+            return false;
+        }
+        return true;
+    }
+
 }
