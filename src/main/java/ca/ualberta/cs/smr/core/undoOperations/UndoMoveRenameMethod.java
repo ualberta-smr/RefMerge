5c999fb92a844ec30b0d6d353d9bb759839cc65d
Max Ellis <mjellis@ualberta.ca>
2021-06-21 14:28:49 -0700

    Move the method to its original location within the class using the text offset

--- src/main/java/ca/ualberta/cs/smr/core/undoOperations/UndoMoveRenameMethod.java
+++ src/main/java/ca/ualberta/cs/smr/core/undoOperations/UndoMoveRenameMethod.java
@@ -3,6 +3,7 @@ package ca.ualberta.cs.smr.core.undoOperations;
 import ca.ualberta.cs.smr.core.refactoringObjects.*;
 import ca.ualberta.cs.smr.core.refactoringObjects.typeObjects.MethodSignatureObject;
 import ca.ualberta.cs.smr.utils.Utils;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.*;
@@ -56,9 +57,74 @@ public class UndoMoveRenameMethod {
                     originalClassName, visibility);
             UsageInfo[] refactoringUsages = moveMethodRefactoring.findUsages();
             moveMethodRefactoring.doRefactoring(refactoringUsages);
+            psiClass = moveMethodRefactoring.getTargetClass();
+            // If the method was not moved to the correct location within the class, move it to the correct location
+            moveMethodToOriginalLocation(psiClass, psiMethod,  moveRenameMethodObject.getStartOffset());
         }
         // Update the virtual file that contains the refactoring
         vFile.refresh(false, true);
+
+    }
+
+    /*
+     * Move the method to the correct location within the class using the text offset detected by RefMiner.
+     */
+    private void moveMethodToOriginalLocation(PsiClass psiClass, PsiMethod psiMethod,  int startOffset) {
+        // Get all of the methods inside of the class.
+        PsiMethod[] psiMethods = psiClass.getMethods();
+
+        if(!psiClass.isValid() || !psiMethod.isValid()) {
+            return;
+        }
+
+        // Get the physical copy of the PSI method so we can delete it
+        for(PsiMethod method : psiMethods) {
+            if(method.getSignature(PsiSubstitutor.UNKNOWN).equals(psiMethod.getSignature(PsiSubstitutor.UNKNOWN))) {
+                psiMethod = method;
+                break;
+            }
+        }
+        PsiMethod psiMethodBefore = null;
+        // Find which method comes before the moved method
+        for(PsiMethod otherMethod : psiMethods) {
+            int otherMethodStartOffset = otherMethod.getTextOffset();
+            int otherMethodEndOffset = otherMethod.getTextLength() + otherMethodStartOffset;
+            if(otherMethodEndOffset < startOffset) {
+                psiMethodBefore = otherMethod;
+            }
+        }
+        final PsiMethod newMethod = PsiElementFactory.getInstance(project).createMethodFromText(psiMethod.getText(), psiClass);
+        PsiMethod finalPsiMethod = psiMethod;
+        // if it's the first method in the class
+        PsiMethod psiMethodAfter = null;
+        if(psiMethodBefore == null) {
+            for(PsiMethod otherMethod : psiMethods) {
+                if(!otherMethod.isConstructor()) {
+                    psiMethodAfter = otherMethod;
+                    break;
+                }
+            }
+            PsiMethod finalPsiMethodAfter = psiMethodAfter;
+            WriteCommandAction.runWriteCommandAction(project, () -> {
+                psiClass.addAfter(newMethod, finalPsiMethodAfter);
+                finalPsiMethod.delete();
+            });
+            return;
+        }
+        PsiMethod finalPsiMethodBefore = psiMethodBefore;
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            psiClass.addAfter(newMethod, finalPsiMethodBefore);
+            finalPsiMethod.delete();
+        });
+        psiMethods = psiClass.getMethods();
+        for(PsiMethod m : psiMethods) {
+            System.out.println(m.getName());
+        }
+
+        psiMethods = psiClass.getMethods();
+        for(PsiMethod m : psiMethods) {
+            System.out.println(m.getName());
+        }
     }
 
 }
