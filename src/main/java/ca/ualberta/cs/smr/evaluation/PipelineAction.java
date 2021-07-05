e1c044f3b095959e4e5aa7865c952b83432722a9
Max Ellis <mjellis@ualberta.ca>
2021-07-05 13:33:41 -0700

    Remove IntelliMerge from evaluation pipeline for now

--- src/main/java/ca/ualberta/cs/smr/evaluation/PipelineAction.java
+++ src/main/java/ca/ualberta/cs/smr/evaluation/PipelineAction.java
@@ -6,7 +6,6 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 
 import com.intellij.vcs.log.Hash;
-//import edu.pku.intellimerge.client.APIClient;
 import git4idea.GitCommit;
 import git4idea.repo.GitRepository;
 import git4idea.repo.GitRepositoryManager;
@@ -29,7 +28,6 @@ public class PipelineAction extends AnAction {
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-//        PipelineAction eval = new PipelineAction();
         this.project = ProjectManager.getInstance().getOpenProjects()[0];
         GitRepositoryManager repoManager = GitRepositoryManager.getInstance(project);
         List<GitRepository> repos = repoManager.getRepositories();
@@ -51,8 +49,8 @@ public class PipelineAction extends AnAction {
         File repoDir = new File(clonedDest);
         GitUtils git = new GitUtils(repo, project);
 //        String mergeCommit = "e34f03bd0c7c805789bdb9da427db7334e61cedc"; // deeplearning4j
-//        String mergeCommit = "588def5f5d92ba1e4ec5929dcaed4150a925a90b"; //undertow
-        String mergeCommit = "07559b47674594fdf40f2855f83b492f67f9093c";
+        String mergeCommit = "588def5f5d92ba1e4ec5929dcaed4150a925a90b"; //undertow
+//        String mergeCommit = "07559b47674594fdf40f2855f83b492f67f9093c"; //error-prone
         git.checkout(mergeCommit);
         Utils.reparsePsiFiles(project);
         Utils.dumbServiceHandler(project);
@@ -66,14 +64,11 @@ public class PipelineAction extends AnAction {
             if(commitHash.equals(mergeCommit)) {
                 // Perform the merge with the three tools.
                 List<Hash> parents = gitCommit.getParents();
-                String leftParent = parents.get(0).toShortString();
-                String rightParent = parents.get(1).toShortString();
+                String rightParent = parents.get(0).toShortString();
+                String leftParent = parents.get(1).toShortString();
                 String baseCommit = git.getBaseCommit(leftParent, rightParent);
                 long refMergeRuntime = runRefMerge(project, repo, leftParent, rightParent);
-//                long intelliMergeRuntime = runIntelliMerge(git, mergeCommit, leftParent, rightParent,
-//                                                            baseCommit, repo.getPresentableUrl());
                 System.out.println("Elapsed RefMerge runtime = " + refMergeRuntime);
-//                System.out.println("Elapsed IntelliMerge runtime = " + intelliMergeRuntime);
 
             }
         }
@@ -94,34 +89,6 @@ public class PipelineAction extends AnAction {
         return time2 - time;
     }
 
-    /*
-     * Merge the directories in the order <left> <base> <right> with IntelliMerge. Return how long it takes for IntelliMerge
-     * to finish.
-     */
-//    private long runIntelliMerge(GitUtils git, String mergeCommit, String leftParent, String rightParent, String baseCommit,
-//                                 String url ) {
-//        git.checkout(leftParent);
-//        List<String> directories = new ArrayList<>();
-//        git.checkout(mergeCommit);
-//        String path = System.getProperty("user.home") + "/temp/" + project.getName();
-//        Utils.saveContent(project, project.getName() + "/ours/");
-//        git.checkout(baseCommit);
-//        Utils.saveContent(project, project.getName() + "/base/");
-//        git.checkout(rightParent);
-//        Utils.saveContent(project, project.getName() + "/theirs/");
-//        String outputPath = System.getProperty("user.home") + "/temp/intelliMergeResults";
-//        APIClient apiClient = new APIClient(project.getName(), path + "/temp", url, outputPath + "/temp", outputPath, true);
-//        try {
-//            long time = System.currentTimeMillis();
-//            apiClient.processDirectory(path, outputPath);
-//            long time2 = System.currentTimeMillis();
-//            return time2 - time;
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        return -1;
-//    }
 
 
 
