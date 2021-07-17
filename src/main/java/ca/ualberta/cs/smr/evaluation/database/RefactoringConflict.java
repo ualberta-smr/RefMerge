d11c5333bc885148ca6c80fc3ad3690fc2989b0d
Max Ellis <mjellis@ualberta.ca>
2021-07-17 03:04:23 -0700

    Add RefMerge refactoring conflicts to database and get IntelliMerge dataset

---
+++ src/main/java/ca/ualberta/cs/smr/evaluation/database/RefactoringConflict.java
@@ -0,0 +1,19 @@
+package ca.ualberta.cs.smr.evaluation.database;
+
+import ca.ualberta.cs.smr.core.refactoringObjects.RefactoringObject;
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Table;
+
+@Table("refactoring_conflict")
+public class RefactoringConflict extends Model {
+
+    public RefactoringConflict() {}
+
+    public RefactoringConflict(RefactoringObject left, RefactoringObject right, MergeResult mergeResult) {
+        set("left_old_path", left.getOriginalFilePath(), "right_old_path", right.getOriginalFilePath(),
+                "left_new_path", left.getDestinationFilePath(), "right_new_path", right.getDestinationFilePath(),
+                "left_refactoring_type", left.getRefactoringType(), "right_refactoring_type", right.getRefactoringType(),
+                "merge_result_id", mergeResult.getId(), "merge_commit_id", mergeResult.getMergeCommitId(),
+                "project_id", mergeResult.getProjectId());
+    }
+}
