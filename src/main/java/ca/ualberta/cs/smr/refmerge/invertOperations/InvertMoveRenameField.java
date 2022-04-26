8113e9b03adbb0692b5c3566d53ac16658d632d5
Max Ellis <mjellis@ualberta.ca>
2022-04-26 13:36:33 -0700

    Add Move Attribute and Move And Rename Attribute to InvertMoveRenameField

--- src/main/java/ca/ualberta/cs/smr/refmerge/invertOperations/InvertMoveRenameField.java
+++ src/main/java/ca/ualberta/cs/smr/refmerge/invertOperations/InvertMoveRenameField.java
@@ -7,7 +7,9 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiMember;
 import com.intellij.refactoring.JavaRefactoringFactory;
+import com.intellij.refactoring.MoveMembersRefactoring;
 import com.intellij.refactoring.RefactoringFactory;
 import com.intellij.refactoring.RenameRefactoring;
 import com.intellij.usageView.UsageInfo;
@@ -26,26 +28,32 @@ public class InvertMoveRenameField {
      * Invert the move and rename field refactorings that was performed in the commit
      */
     public void invertRenameField(RefactoringObject ref) {
-        MoveRenameFieldObject renameFieldObject = (MoveRenameFieldObject) ref;
+        MoveRenameFieldObject fieldObject = (MoveRenameFieldObject) ref;
         // The field name we are inverting to
-        String originalField = renameFieldObject.getOriginalName();
+        String originalField = fieldObject.getOriginalName();
         // The field name we are inverting
-        String renamedField = renameFieldObject.getDestinationName();
+        String renamedField = fieldObject.getDestinationName();
 
         // The file and class that we are inverting the refactoring in. We use the original instead of the destination
         // because the class refactorings were already inverted.
-        String originalFile = renameFieldObject.getOriginalFilePath();
-        String originalClass = renameFieldObject.getOriginalClass();
+        String originalFile = fieldObject.getOriginalFilePath();
+        String originalClass = fieldObject.getOriginalClass();
+        String destinationFile = fieldObject.getDestinationFilePath();
+        String destinationClass = fieldObject.getDestinationClass();
 
         Utils utils = new Utils(project);
         utils.addSourceRoot(originalFile, originalClass);
 
         PsiClass psiClass = null;
-        // Set up for combining Rename Field, Move Field, and Rename + Move Field
-        if(renameFieldObject.isRename()) {
+        // If it is a Move Field or Rename+Move Field refactoring, use the destination class
+        if(fieldObject.isMove()) {
+            psiClass = utils.getPsiClassFromClassAndFileNames(destinationClass, destinationFile);
+        }
+        else if(fieldObject.isRename()) {
             psiClass = utils.getPsiClassFromClassAndFileNames(originalClass, originalFile);
 
         }
+
         // If we cannot find the PSI class, do not invert the refactoring
         if(psiClass == null) {
             System.out.println("Could not find PSI Class for " + originalClass);
@@ -55,12 +63,30 @@ public class InvertMoveRenameField {
         VirtualFile virtualFile = psiClass.getContainingFile().getVirtualFile();
 
         PsiField psiField = Utils.getPsiField(psiClass, renamedField);
+        // If the field object is a rename field, perform the rename field first
+        if(fieldObject.isRename()) {
+            RefactoringFactory factory = JavaRefactoringFactory.getInstance(project);
+            assert psiField != null;
+            RenameRefactoring renameRefactoring = factory.createRename(psiField, originalField, true, true);
+            UsageInfo[] refactoringUsages = renameRefactoring.findUsages();
+            renameRefactoring.doRefactoring(refactoringUsages);
+        }
+        // Now, if the field was moved, undo the move by performing a move field refactoring to move it to the original class
+        if(fieldObject.isMove()) {
+            JavaRefactoringFactory refactoringFactory = JavaRefactoringFactory.getInstance(project);
+            String visibility = fieldObject.getVisibility();
+            PsiMember[] psiMembers = new PsiMember[1];
+            psiMembers[0] = psiField;
+            MoveMembersRefactoring moveFieldRefactoring = refactoringFactory.createMoveMembers(psiMembers,
+                    originalClass, visibility);
+            UsageInfo[] refactoringUsages = moveFieldRefactoring.findUsages();
+            moveFieldRefactoring.doRefactoring(refactoringUsages);
+            psiClass = moveFieldRefactoring.getTargetClass();
+            if(psiClass == null) {
+                return;
+            }
+        }
 
-        RefactoringFactory factory = JavaRefactoringFactory.getInstance(project);
-        assert psiField != null;
-        RenameRefactoring renameRefactoring = factory.createRename(psiField, originalField, true, true);
-        UsageInfo[] refactoringUsages = renameRefactoring.findUsages();
-        renameRefactoring.doRefactoring(refactoringUsages);
         // Check if there is a usage view. if so, close the usage view and do not perform the refactoring.
         // Need to check if there is a workaround or solution for this
         UsageViewManager viewManager = UsageViewManager.getInstance(project);
