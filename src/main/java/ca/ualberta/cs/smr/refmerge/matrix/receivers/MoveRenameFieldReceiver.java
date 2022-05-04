aab3f8e90cd8d2e769314c5f809f6e653d3163b0
Max Ellis <mjellis@ualberta.ca>
2022-05-04 13:50:45 -0700

    Add MoveRenameField Dependence handler

--- src/main/java/ca/ualberta/cs/smr/refmerge/matrix/receivers/MoveRenameFieldReceiver.java
+++ src/main/java/ca/ualberta/cs/smr/refmerge/matrix/receivers/MoveRenameFieldReceiver.java
@@ -3,6 +3,7 @@ package ca.ualberta.cs.smr.refmerge.matrix.receivers;
 import ca.ualberta.cs.smr.refmerge.matrix.dispatcher.*;
 import ca.ualberta.cs.smr.refmerge.matrix.logicCells.MoveRenameFieldMoveRenameClassCell;
 import ca.ualberta.cs.smr.refmerge.matrix.logicCells.MoveRenameFieldMoveRenameFieldCell;
+import ca.ualberta.cs.smr.refmerge.refactoringObjects.MoveRenameFieldObject;
 import ca.ualberta.cs.smr.refmerge.refactoringObjects.RefactoringObject;
 
 
@@ -24,8 +25,34 @@ public class MoveRenameFieldReceiver extends Receiver {
             if(isConflicting) {
                 dispatcherObject.setReplayFlag(false);
                 this.refactoringObject.setReplayFlag(false);
+                System.out.println("Rename Field/Rename Field conflict: " + isConflicting);
             }
-            System.out.println("Rename Field/Rename Field conflict: " + isConflicting);
+            if(!isConflicting) {
+                boolean isDependent = cell.checkDependence(dispatcherObject, this.refactoringObject);
+                if(isDependent) {
+                    // If the dispatcher refactoring is the rename method operation
+                    if(((MoveRenameFieldObject) dispatcherObject).isRename()) {
+                        // Set the renamed field's class and file to the moved field's class and file
+                        dispatcherObject.setDestinationFilePath(this.refactoringObject.getDestinationFilePath());
+                        ((MoveRenameFieldObject) dispatcherObject)
+                                .setDestinationClassName(((MoveRenameFieldObject) this.refactoringObject).getDestinationClass());
+                        ((MoveRenameFieldObject) dispatcherObject)
+                                .setDestinationFieldName(((MoveRenameFieldObject) this.refactoringObject).getDestinationName());
+                    }
+                    // Otherwise, if the receiver refactoring is the rename field operation
+                    else {
+                        this.refactoringObject.setDestinationFilePath(dispatcherObject.getDestinationFilePath());
+                        ((MoveRenameFieldObject) this.refactoringObject)
+                                .setDestinationClassName(((MoveRenameFieldObject) dispatcherObject).getDestinationClass());
+                        // Set the moved method's signature to the renamed method's signature
+                        ((MoveRenameFieldObject) dispatcherObject)
+                                .setDestinationFieldName(((MoveRenameFieldObject) this.refactoringObject)
+                                        .getDestinationName());
+                    }
+                    dispatcherObject.setReplayFlag(false);
+                    dispatcher.setRefactoringObject(dispatcherObject);
+                }
+            }
         }
     }
 
