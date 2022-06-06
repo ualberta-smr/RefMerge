ca93f12bb5f272c5d326e68560aa06c411224191
Max Ellis <mjellis@ualberta.ca>
2022-06-06 12:16:27 -0700

    Add pull up method receiver placeholder

---
+++ src/main/java/ca/ualberta/cs/smr/refmerge/matrix/receivers/PullUpMethodReceiver.java
@@ -0,0 +1,10 @@
+package ca.ualberta.cs.smr.refmerge.matrix.receivers;
+
+import ca.ualberta.cs.smr.refmerge.matrix.dispatcher.PullUpMethodDispatcher;
+
+public class PullUpMethodReceiver extends Receiver {
+
+    public void receive(PullUpMethodDispatcher dispatcher) {
+
+    }
+}
