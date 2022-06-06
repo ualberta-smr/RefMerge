c307d7597bde927e3f5d7e8efaabac9923504218
Max Ellis <mjellis@ualberta.ca>
2022-06-06 16:28:00 -0700

    Check override conflict for pull up method/move+rename method

--- src/main/java/ca/ualberta/cs/smr/refmerge/matrix/logicCells/PullUpMethodMoveRenameMethodCell.java
+++ src/main/java/ca/ualberta/cs/smr/refmerge/matrix/logicCells/PullUpMethodMoveRenameMethodCell.java
@@ -4,7 +4,12 @@ import ca.ualberta.cs.smr.refmerge.refactoringObjects.MoveRenameMethodObject;
 import ca.ualberta.cs.smr.refmerge.refactoringObjects.PullUpMethodObject;
 import ca.ualberta.cs.smr.refmerge.refactoringObjects.RefactoringObject;
 import ca.ualberta.cs.smr.refmerge.refactoringObjects.typeObjects.MethodSignatureObject;
+import ca.ualberta.cs.smr.refmerge.utils.Utils;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiClass;
+
+import static ca.ualberta.cs.smr.refmerge.utils.MatrixUtils.ifClassExtends;
+import static ca.ualberta.cs.smr.refmerge.utils.MatrixUtils.isSameName;
 
 public class PullUpMethodMoveRenameMethodCell {
     Project project;
@@ -24,13 +29,50 @@ public class PullUpMethodMoveRenameMethodCell {
         // Check for override conflict
 
         // Check for overload conflict
-
-        // Check for naming conflict
-        if(namingConflict(dispatcherObject, receiverObject)) {
+        if(overrideConflict(dispatcherObject, receiverObject)) {
             return true;
         }
+        // Check for naming conflict
+        return namingConflict(dispatcherObject, receiverObject);
+    }
 
-        return false;
+    public boolean overrideConflict(MoveRenameMethodObject dispatcher, PullUpMethodObject receiver) {
+
+        MethodSignatureObject dispatcherOriginalMethod = dispatcher.getOriginalMethodSignature();
+        MethodSignatureObject receiverOriginalMethod = receiver.getOriginalMethodSignature();
+        // Get the refactored operations
+        MethodSignatureObject dispatcherDestinationMethod = dispatcher.getDestinationMethodSignature();
+        MethodSignatureObject receiverDestinationMethod = receiver.getDestinationMethodSignature();
+        // Get the class names
+        String dispatcherClassName = dispatcher.getOriginalClassName();
+        String receiverClassName = receiver.getOriginalClass();
+
+        // If the rename methods happen in the same class then there is no override conflict
+        if(dispatcherClassName.equals(receiverClassName)) {
+            return false;
+        }
+        String dispatcherFile = dispatcher.getOriginalFilePath();
+        String receiverFile = receiver.getOriginalFilePath();
+        Utils utils = new Utils(project);
+        PsiClass psiDispatcher = utils.getPsiClassByFilePath(dispatcherFile, dispatcherClassName);
+        PsiClass psiReceiver = utils.getPsiClassByFilePath(receiverFile, receiverClassName);
+        if(psiReceiver != null && psiDispatcher != null) {
+            if (!ifClassExtends(psiDispatcher, psiReceiver)) {
+                return false;
+            }
+        }
+        // Get original method names
+        String dispatcherOriginalMethodName = dispatcherOriginalMethod.getName();
+        String receiverOriginalMethodName = receiverOriginalMethod.getName();
+        // get new method names
+        String dispatcherNewMethodName = dispatcherDestinationMethod.getName();
+        String receiverNewMethodName = receiverDestinationMethod.getName();
+        // Check if the methods end with the same name and start with different names.
+        // If they do, then there's a likely override conflict.
+        return !isSameName(dispatcherOriginalMethodName, receiverOriginalMethodName) &&
+                isSameName(dispatcherNewMethodName, receiverNewMethodName) &&
+                dispatcherDestinationMethod.equalsSignature(receiverDestinationMethod);
+
     }
 
     public boolean namingConflict(MoveRenameMethodObject dispatcher, PullUpMethodObject receiver) {
--- src/test/java/ca/ualberta/cs/smr/refmerge/matrix/logicCells/PullUpMethodMoveRenameMethodTest.java
+++ src/test/java/ca/ualberta/cs/smr/refmerge/matrix/logicCells/PullUpMethodMoveRenameMethodTest.java
@@ -2,13 +2,23 @@ package ca.ualberta.cs.smr.refmerge.matrix.logicCells;
 
 import ca.ualberta.cs.smr.refmerge.refactoringObjects.MoveRenameMethodObject;
 import ca.ualberta.cs.smr.refmerge.refactoringObjects.PullUpMethodObject;
+import ca.ualberta.cs.smr.refmerge.refactoringObjects.RefactoringObject;
 import ca.ualberta.cs.smr.refmerge.refactoringObjects.typeObjects.MethodSignatureObject;
+import ca.ualberta.cs.smr.testUtils.GetDataForTests;
+import com.intellij.openapi.project.Project;
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
+import gr.uom.java.xmi.diff.RenameOperationRefactoring;
 import org.junit.Assert;
+import org.refactoringminer.api.Refactoring;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class PullUpMethodMoveRenameMethodTest extends LightJavaCodeInsightFixtureTestCase {
+
+    protected String getTestDataPath() {
+        return "src/test/resources";
+    }
 
     public void testNamingConflict() {
         MethodSignatureObject bar = new MethodSignatureObject(new ArrayList<>(), "bar");
@@ -32,6 +42,45 @@ public class PullUpMethodMoveRenameMethodTest extends LightJavaCodeInsightFixtur
         Assert.assertTrue(isConflicting);
         isConflicting = cell.conflictCell(moveRenameMethodObject3, pullUpMethodObject1);
         Assert.assertFalse(isConflicting);
+    }
+
+    public void testOverrideConflict() {
+        Project project = myFixture.getProject();
+        String basePath = System.getProperty("user.dir");
+        // Reuse rename method files to get PSI structure for override test
+        String originalPath = basePath + "/src/test/resources/renameMethodRenameMethodFiles/methodOverrideConflict/original";
+        String refactoredPath = basePath + "/src/test/resources/renameMethodRenameMethodFiles/methodOverrideConflict/refactored";
+        String configurePath = "renameMethodRenameMethodFiles/methodOverrideConflict/original/Override.java";
+        myFixture.configureByFiles(configurePath);
+
+        List<Refactoring> refactorings = GetDataForTests.getRefactorings("RENAME_METHOD", originalPath, refactoredPath);
+        assert refactorings != null;
+        assert refactorings.size() == 5;
+        MoveRenameMethodObject renameObject = null;
+        MoveRenameMethodObject pullUpObject = null;
+        for(Refactoring refactoring : refactorings) {
+            String originalName = ((RenameOperationRefactoring) refactoring).getOriginalOperation().getName();
+            String newName = ((RenameOperationRefactoring) refactoring).getRenamedOperation().getName();
+            if(originalName.equals("addNumbers") && newName.equals("numbers")) {
+                renameObject = new MoveRenameMethodObject(refactoring);
+            }
+            if(originalName.equals("doNumbers") && newName.equals("numbers")) {
+                pullUpObject = new MoveRenameMethodObject(refactoring);
+            }
+        }
+
+        PullUpMethodObject pullUpMethodObject = new PullUpMethodObject("ChildClass", "doNumbers", "ChildClass", "numbers");
+
+        assert pullUpObject != null;
+        pullUpMethodObject.setDestinationFilePath(pullUpObject.getDestinationFilePath());
+        pullUpMethodObject.setOriginalMethodSignature(pullUpObject.getOriginalMethodSignature());
+        pullUpMethodObject.setDestinationMethodSignature(pullUpObject.getDestinationMethodSignature());
+        pullUpMethodObject.setOriginalFilePath(pullUpObject.getOriginalFilePath());
+
+        PullUpMethodMoveRenameMethodCell cell = new PullUpMethodMoveRenameMethodCell(project);
+        assert renameObject != null;
+        boolean isConflict = cell.overrideConflict(renameObject, pullUpMethodObject);
+        Assert.assertTrue(isConflict);
     }
 
 }
