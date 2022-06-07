39174de172e4d439dfc1b1f535375cfc86a96199
Max Ellis <mjellis@ualberta.ca>
2022-06-07 15:34:21 -0700

    Check for pull up method/inline method naming conflict

---
+++ src/main/java/ca/ualberta/cs/smr/refmerge/matrix/logicCells/PullUpMethodInlineMethodCell.java
@@ -0,0 +1,41 @@
+package ca.ualberta.cs.smr.refmerge.matrix.logicCells;
+
+import ca.ualberta.cs.smr.refmerge.refactoringObjects.InlineMethodObject;
+import ca.ualberta.cs.smr.refmerge.refactoringObjects.PullUpMethodObject;
+import ca.ualberta.cs.smr.refmerge.refactoringObjects.RefactoringObject;
+import ca.ualberta.cs.smr.refmerge.refactoringObjects.typeObjects.MethodSignatureObject;
+import com.intellij.openapi.project.Project;
+
+public class PullUpMethodInlineMethodCell {
+
+    Project project;
+
+    public PullUpMethodInlineMethodCell(Project project) {
+        this.project = project;
+    }
+
+    public boolean conflictCell(RefactoringObject dispatcher, RefactoringObject receiver) {
+        InlineMethodObject inlineMethodObject = (InlineMethodObject) dispatcher;
+        PullUpMethodObject pullUpMethodObject = (PullUpMethodObject) receiver;
+
+        // Naming conflict check
+        return namingConflict(inlineMethodObject, pullUpMethodObject);
+    }
+
+    public boolean namingConflict(InlineMethodObject dispatcher, PullUpMethodObject receiver) {
+        String dispatcherOriginalClass = dispatcher.getOriginalClassName();
+        String receiverOriginalClass = receiver.getOriginalClass();
+
+        // Only need to get the destination signatures because the extract method refactoring creates a new
+        // program element. The original signature for extract method is the signature the extract method
+        // refactoring comes from
+        MethodSignatureObject dispatcherOriginalMethod = dispatcher.getOriginalMethodSignature();
+        MethodSignatureObject receiverOriginalMethod = receiver.getOriginalMethodSignature();
+
+
+        // If two methods are pulled up from and inlined from the same method, it is conflicting
+        return dispatcherOriginalClass.equals(receiverOriginalClass)
+                && dispatcherOriginalMethod.equalsSignature(receiverOriginalMethod);
+    }
+
+}
