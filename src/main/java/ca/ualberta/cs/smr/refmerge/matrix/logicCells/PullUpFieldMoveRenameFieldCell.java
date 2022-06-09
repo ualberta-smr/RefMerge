05b2ab62bf69d0d0129bf65f3e7904754db4ea83
Max Ellis <mjellis@ualberta.ca>
2022-06-09 13:56:02 -0700

    Add pull up field / move + rename field accidental shadow conflict check

--- src/main/java/ca/ualberta/cs/smr/refmerge/matrix/logicCells/PullUpFieldMoveRenameFieldCell.java
+++ src/main/java/ca/ualberta/cs/smr/refmerge/matrix/logicCells/PullUpFieldMoveRenameFieldCell.java
@@ -3,7 +3,12 @@ package ca.ualberta.cs.smr.refmerge.matrix.logicCells;
 import ca.ualberta.cs.smr.refmerge.refactoringObjects.MoveRenameFieldObject;
 import ca.ualberta.cs.smr.refmerge.refactoringObjects.PullUpFieldObject;
 import ca.ualberta.cs.smr.refmerge.refactoringObjects.RefactoringObject;
+import ca.ualberta.cs.smr.refmerge.utils.Utils;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiClass;
+
+import static ca.ualberta.cs.smr.refmerge.utils.MatrixUtils.ifClassExtends;
+import static ca.ualberta.cs.smr.refmerge.utils.MatrixUtils.isSameName;
 
 public class PullUpFieldMoveRenameFieldCell {
     Project project;
@@ -16,8 +21,42 @@ public class PullUpFieldMoveRenameFieldCell {
         MoveRenameFieldObject moveRenameFieldObject = (MoveRenameFieldObject) dispatcher;
         PullUpFieldObject pullUpFieldObject = (PullUpFieldObject) receiver;
         // Shadow conflict
+        if(shadowConflict(moveRenameFieldObject, pullUpFieldObject)) {
+            return true;
+        }
         // Naming conflict
         return namingConflict(moveRenameFieldObject, pullUpFieldObject);
+    }
+
+    public boolean shadowConflict(MoveRenameFieldObject dispatcher, PullUpFieldObject receiver) {
+
+        String newDispatcherClass = dispatcher.getDestinationClass();
+        String newReceiverClass = receiver.getTargetClass();
+
+        // Cannot have shadow conflict in same class
+        if(newDispatcherClass.equals(newReceiverClass)) {
+            return false;
+        }
+
+        String dispatcherFile = dispatcher.getDestinationFilePath();
+        String receiverFile = receiver.getDestinationFilePath();
+        Utils utils = new Utils(project);
+        PsiClass psiDispatcher = utils.getPsiClassByFilePath(dispatcherFile, newDispatcherClass);
+        PsiClass psiReceiver = utils.getPsiClassByFilePath(receiverFile, newReceiverClass);
+        if(psiReceiver != null && psiDispatcher != null) {
+            // If there is no inheritance relationship, there is no shadow conflict
+            if (!ifClassExtends(psiDispatcher, psiReceiver)) {
+                return false;
+            }
+        }
+
+        String newDispatcherField = dispatcher.getDestinationName();
+        String newReceiverField = receiver.getRefactoredFieldName();
+
+        // The original name does not matter in this case
+        return isSameName(newDispatcherField, newReceiverField);
+
+
     }
 
     public boolean namingConflict(MoveRenameFieldObject dispatcher, PullUpFieldObject receiver) {
