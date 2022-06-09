3b425d3333bd8867f34456f7e64761b087f2445b
Max Ellis <mjellis@ualberta.ca>
2022-06-09 14:55:10 -0700

    Dispatch to push down field / push down field

--- src/main/java/ca/ualberta/cs/smr/refmerge/matrix/receivers/PushDownFieldReceiver.java
+++ src/main/java/ca/ualberta/cs/smr/refmerge/matrix/receivers/PushDownFieldReceiver.java
@@ -1,12 +1,32 @@
 package ca.ualberta.cs.smr.refmerge.matrix.receivers;
 
 import ca.ualberta.cs.smr.refmerge.matrix.dispatcher.PushDownFieldDispatcher;
+import ca.ualberta.cs.smr.refmerge.matrix.logicCells.PushDownFieldPushDownFieldCell;
+import ca.ualberta.cs.smr.refmerge.refactoringObjects.RefactoringObject;
 
 public class PushDownFieldReceiver extends Receiver {
 
+    /*
+     * Check for push down field / push down field naming and accidental shadowing conflicts. If on the same branch, check
+     * for transitivity.
+     */
     @Override
     public void receive(PushDownFieldDispatcher dispatcher) {
+        RefactoringObject dispatcherRefactoring = dispatcher.getRefactoringObject();
+        PushDownFieldPushDownFieldCell cell = new PushDownFieldPushDownFieldCell(project);
+        if(dispatcher.isSimplify()) {
+            // Dispatcher refactoring is always the second refactoring when dealing with two refactorings of the same type
+            this.isTransitive = cell.checkTransitivity(this.refactoringObject, dispatcherRefactoring);
+            dispatcher.setRefactoringObject(dispatcherRefactoring);
+        }
 
+        if(!dispatcher.isSimplify()) {
+            boolean isConflicting = cell.conflictCell(dispatcherRefactoring, this.refactoringObject);
+            if(isConflicting) {
+                dispatcherRefactoring.setReplayFlag(false);
+                this.refactoringObject.setReplayFlag(false);
+            }
+        }
     }
 
 }
