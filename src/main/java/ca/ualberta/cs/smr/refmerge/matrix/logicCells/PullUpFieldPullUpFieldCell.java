80c688302e04dd914f8be01822386ca7e77c64bc
Max Ellis <mjellis@ualberta.ca>
2022-06-09 13:24:48 -0700

    Add check for accidental shadowing conflicts for pull up field / pull up field

--- src/main/java/ca/ualberta/cs/smr/refmerge/matrix/logicCells/PullUpFieldPullUpFieldCell.java
+++ src/main/java/ca/ualberta/cs/smr/refmerge/matrix/logicCells/PullUpFieldPullUpFieldCell.java
@@ -2,7 +2,12 @@ package ca.ualberta.cs.smr.refmerge.matrix.logicCells;
 
 import ca.ualberta.cs.smr.refmerge.refactoringObjects.PullUpFieldObject;
 import ca.ualberta.cs.smr.refmerge.refactoringObjects.RefactoringObject;
+import ca.ualberta.cs.smr.refmerge.utils.Utils;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiClass;
+
+import static ca.ualberta.cs.smr.refmerge.utils.MatrixUtils.ifClassExtends;
+import static ca.ualberta.cs.smr.refmerge.utils.MatrixUtils.isSameName;
 
 public class PullUpFieldPullUpFieldCell {
 
@@ -16,8 +21,41 @@ public class PullUpFieldPullUpFieldCell {
         PullUpFieldObject dispatcherObject = (PullUpFieldObject) dispatcher;
         PullUpFieldObject receiverObject = (PullUpFieldObject) receiver;
         // Accidental shadow conflict
+        if(shadowConflict(dispatcherObject, receiverObject)) {
+            return  true;
+        }
         // Naming conflict
         return namingConflict(dispatcherObject, receiverObject);
+    }
+
+    public boolean shadowConflict(PullUpFieldObject dispatcherObject, PullUpFieldObject receiverObject) {
+
+        String newDispatcherClass = dispatcherObject.getTargetClass();
+        String newReceiverClass = receiverObject.getTargetClass();
+
+        // Cannot have shadow conflict in same class
+        if(newDispatcherClass.equals(newReceiverClass)) {
+            return false;
+        }
+
+        String dispatcherFile = dispatcherObject.getDestinationFilePath();
+        String receiverFile = receiverObject.getDestinationFilePath();
+        Utils utils = new Utils(project);
+        PsiClass psiDispatcher = utils.getPsiClassByFilePath(dispatcherFile, newDispatcherClass);
+        PsiClass psiReceiver = utils.getPsiClassByFilePath(receiverFile, newReceiverClass);
+        if(psiReceiver != null && psiDispatcher != null) {
+            // If there is no inheritance relationship, there is no shadow conflict
+            if (!ifClassExtends(psiDispatcher, psiReceiver)) {
+                return false;
+            }
+        }
+
+        String newDispatcherField = dispatcherObject.getRefactoredFieldName();
+        String newReceiverField = receiverObject.getRefactoredFieldName();
+
+        // The original name does not matter in this case
+        return isSameName(newDispatcherField, newReceiverField);
+
     }
 
     public boolean namingConflict(PullUpFieldObject dispatcher, PullUpFieldObject receiver) {
--- src/test/java/ca/ualberta/cs/smr/refmerge/matrix/logicCells/PullUpFieldPullUpFieldTest.java
+++ src/test/java/ca/ualberta/cs/smr/refmerge/matrix/logicCells/PullUpFieldPullUpFieldTest.java
@@ -1,10 +1,16 @@
 package ca.ualberta.cs.smr.refmerge.matrix.logicCells;
 
 import ca.ualberta.cs.smr.refmerge.refactoringObjects.PullUpFieldObject;
+import com.intellij.openapi.project.Project;
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
 import org.junit.Assert;
 
+
 public class PullUpFieldPullUpFieldTest extends LightJavaCodeInsightFixtureTestCase {
+
+    protected String getTestDataPath() {
+        return "src/test/resources";
+    }
 
     public void testNamingConflict() {
         // Ref 1: A.foo pulled up to B.foo
@@ -21,4 +27,24 @@ public class PullUpFieldPullUpFieldTest extends LightJavaCodeInsightFixtureTestC
         isConflicting = cell.conflictCell(pullUpFieldObject2, pullUpFieldObject3);
         Assert.assertFalse(isConflicting);
     }
+
+
+    public void testShadowConflict() {
+        Project project = myFixture.getProject();
+        // Reuse field shadowing files to get PSI structure for shadow test
+        String configurePath = "fieldShadowingFiles/Main.java";
+        myFixture.configureByFiles(configurePath);
+
+        PullUpFieldObject pullUpFieldObject = new PullUpFieldObject("Main", "aField", "Main", "aField");
+        pullUpFieldObject.setOriginalFilePath(configurePath);
+        pullUpFieldObject.setDestinationFilePath(configurePath);
+        PullUpFieldObject pullUpFieldObject2 = new PullUpFieldObject("SubClass", "aField", "SubClass", "aField");
+        pullUpFieldObject2.setOriginalFilePath(configurePath);
+        pullUpFieldObject2.setDestinationFilePath(configurePath);
+
+        PullUpFieldPullUpFieldCell cell = new PullUpFieldPullUpFieldCell(project);
+        boolean isConflict = cell.shadowConflict(pullUpFieldObject, pullUpFieldObject2);
+        Assert.assertTrue(isConflict);
+    }
+
 }
