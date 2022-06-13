d3f7789589a88cc143efbf9ed5cc75bf9351fd93
Max Ellis <mjellis@ualberta.ca>
2022-06-13 16:27:52 -0700

    Check rename package / inline method combination

---
+++ src/test/java/ca/ualberta/cs/smr/refmerge/matrix/logicCells/RenamePackageInlineMethodTest.java
@@ -0,0 +1,49 @@
+package ca.ualberta.cs.smr.refmerge.matrix.logicCells;
+
+import ca.ualberta.cs.smr.refmerge.refactoringObjects.InlineMethodObject;
+import ca.ualberta.cs.smr.refmerge.refactoringObjects.RenamePackageObject;
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
+import org.junit.Assert;
+
+public class RenamePackageInlineMethodTest extends LightJavaCodeInsightFixtureTestCase {
+
+    public void testCheckCombination() {
+        // p.subP1 -> p.subP2
+        RenamePackageObject renamePackageObject = new RenamePackageObject("p.subP1", "p.subP2");
+
+        // p.subP1.m1 inlined to p.subP1.m2
+        InlineMethodObject inlineMethodObject = new InlineMethodObject("", "p.subP1.C1",
+                null, "", "p.subP1.C1", null);
+
+        boolean isCombination = RenamePackageInlineMethodCell.checkCombination(inlineMethodObject, renamePackageObject);
+        Assert.assertTrue(isCombination);
+        Assert.assertEquals("p.subP2.C1", inlineMethodObject.getOriginalClassName());
+        Assert.assertEquals("p.subP2.C1", inlineMethodObject.getDestinationClassName());
+        // p.subP3.m1 inlined to p.subP1.m2
+        inlineMethodObject = new InlineMethodObject("", "p.subP1.C1",
+                null, "", "p.subP3.C1", null);
+
+        isCombination = RenamePackageInlineMethodCell.checkCombination(inlineMethodObject, renamePackageObject);
+        Assert.assertTrue(isCombination);
+        Assert.assertEquals("p.subP2.C1", inlineMethodObject.getOriginalClassName());
+        Assert.assertNotEquals("p.subP2.C1", inlineMethodObject.getDestinationClassName());
+        // p.subP1.m1 inlined to p.subP3.m2
+        inlineMethodObject = new InlineMethodObject("", "p.subP3.C1",
+                null, "", "p.subP1.C1", null);
+
+        isCombination = RenamePackageInlineMethodCell.checkCombination(inlineMethodObject, renamePackageObject);
+        Assert.assertTrue(isCombination);
+        Assert.assertNotEquals("p.subP2.C1", inlineMethodObject.getOriginalClassName());
+        Assert.assertEquals("p.subP2.C1", inlineMethodObject.getDestinationClassName());
+
+        isCombination = RenamePackageInlineMethodCell.checkCombination(inlineMethodObject, renamePackageObject);
+        // p.subP3.m1 inlined to p.subP3.m2
+        inlineMethodObject = new InlineMethodObject("", "p.subP3.C1",
+                null, "", "p.subP1.C3", null);
+
+        Assert.assertFalse(isCombination);
+        Assert.assertNotEquals("p.subP2.C1", inlineMethodObject.getOriginalClassName());
+        Assert.assertNotEquals("p.subP2.C1", inlineMethodObject.getDestinationClassName());
+
+    }
+}
