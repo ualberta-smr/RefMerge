439eeb848ea94a2111890224fb3a8ebbb7320d6c
Max Ellis <mjellis@ualberta.ca>
2022-06-13 16:46:16 -0700

    Add rename package / move + rename class combination check

---
+++ src/main/java/ca/ualberta/cs/smr/refmerge/matrix/logicCells/RenamePackageMoveRenameClassCell.java
@@ -0,0 +1,31 @@
+package ca.ualberta.cs.smr.refmerge.matrix.logicCells;
+
+import ca.ualberta.cs.smr.refmerge.refactoringObjects.MoveRenameClassObject;
+import ca.ualberta.cs.smr.refmerge.refactoringObjects.RefactoringObject;
+import ca.ualberta.cs.smr.refmerge.refactoringObjects.RenamePackageObject;
+import ca.ualberta.cs.smr.refmerge.refactoringObjects.typeObjects.ClassObject;
+
+public class RenamePackageMoveRenameClassCell {
+
+    public static boolean checkCombination(RefactoringObject dispatcher, RefactoringObject receiver) {
+        MoveRenameClassObject dispatcherObject = (MoveRenameClassObject) dispatcher;
+        RenamePackageObject receiverObject = (RenamePackageObject) receiver;
+
+        ClassObject classObject = dispatcherObject.getDestinationClassObject();
+        String dispatcherDestinationPackageName = classObject.getPackageName();
+
+
+        String receiverOriginalPackageName = receiverObject.getOriginalName();
+        String receiverDestinationPackageName = receiverObject.getDestinationName();
+
+        // If p1.c1 -> p1.c2 before p1.c2 -> p2.c2
+        if(dispatcherDestinationPackageName.contains(receiverOriginalPackageName)) {
+            classObject.setPackageName(receiverDestinationPackageName);
+            // Update the destination package
+            ((MoveRenameClassObject) dispatcher).setDestinationClassObject(classObject);
+            return true;
+        }
+        return false;
+    }
+
+}
