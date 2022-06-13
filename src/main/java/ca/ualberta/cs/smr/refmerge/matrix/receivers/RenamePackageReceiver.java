f34f444f039541e58afbb6fdf89caa5cf4e381c2
Max Ellis <mjellis@ualberta.ca>
2022-06-13 15:46:07 -0700

    Dispatch to rename package / rename package cells

--- src/main/java/ca/ualberta/cs/smr/refmerge/matrix/receivers/RenamePackageReceiver.java
+++ src/main/java/ca/ualberta/cs/smr/refmerge/matrix/receivers/RenamePackageReceiver.java
@@ -1,7 +1,36 @@
 package ca.ualberta.cs.smr.refmerge.matrix.receivers;
 
+import ca.ualberta.cs.smr.refmerge.matrix.dispatcher.RenamePackageDispatcher;
+import ca.ualberta.cs.smr.refmerge.matrix.logicCells.RenamePackageRenamePackageCell;
+import ca.ualberta.cs.smr.refmerge.refactoringObjects.RefactoringObject;
+
 public class RenamePackageReceiver extends Receiver {
 
+    /*
+     * If simplify is true, check for rename package / rename package transitivity. If it is false, check for
+     * rename package / rename package conflict.
+     */
+    @Override
+    public void receive(RenamePackageDispatcher dispatcher) {
+        // Check for rename package / rename package transitivity
+        if(dispatcher.isSimplify()) {
+            RefactoringObject secondRefactoring = dispatcher.getRefactoringObject();
+            // Dispatcher refactoring is always the second refactoring when dealing with two refactorings of the same type
+            this.isTransitive = RenamePackageRenamePackageCell.checkTransitivity(this.refactoringObject, secondRefactoring);
+            dispatcher.setRefactoringObject(secondRefactoring);
+        }
+        // Check for rename package / rename package conflict
+        else {
+            RefactoringObject dispatcherRefactoring = dispatcher.getRefactoringObject();
+            boolean isConflicting = RenamePackageRenamePackageCell
+                    .conflictCell(dispatcherRefactoring, this.refactoringObject);
+            this.isConflicting = isConflicting;
+            if(isConflicting) {
+                dispatcherRefactoring.setReplayFlag(false);
+                this.refactoringObject.setReplayFlag(false);
+            }
 
+        }
+    }
 
 }
