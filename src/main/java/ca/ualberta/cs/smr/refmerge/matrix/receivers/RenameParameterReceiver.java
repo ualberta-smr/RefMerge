7923fe78585cd76097d7f0ee98ce15f1ef3d83a8
Max Ellis <mjellis@ualberta.ca>
2022-06-14 15:40:05 -0700

    Add rename parameter / rename package combination cell and dispatch to it

--- src/main/java/ca/ualberta/cs/smr/refmerge/matrix/receivers/RenameParameterReceiver.java
+++ src/main/java/ca/ualberta/cs/smr/refmerge/matrix/receivers/RenameParameterReceiver.java
@@ -1,6 +1,8 @@
 package ca.ualberta.cs.smr.refmerge.matrix.receivers;
 
+import ca.ualberta.cs.smr.refmerge.matrix.dispatcher.RenamePackageDispatcher;
 import ca.ualberta.cs.smr.refmerge.matrix.dispatcher.RenameParameterDispatcher;
+import ca.ualberta.cs.smr.refmerge.matrix.logicCells.RenameParameterRenamePackageCell;
 import ca.ualberta.cs.smr.refmerge.matrix.logicCells.RenameParameterRenameParameterCell;
 import ca.ualberta.cs.smr.refmerge.refactoringObjects.RefactoringObject;
 
@@ -26,6 +28,19 @@ public class RenameParameterReceiver extends Receiver {
                 secondRefactoring.setReplayFlag(false);
                 this.refactoringObject.setReplayFlag(false);
             }
+        }
+    }
+
+    /*
+     * If simplify is true, check for rename parameter / rename package combination. If it is false, there is no
+     * conflict to check for between package and parameter level
+     */
+    @Override
+    public void receive(RenamePackageDispatcher dispatcher) {
+        if(dispatcher.isSimplify()) {
+            RefactoringObject dispatcherRefactoring = dispatcher.getRefactoringObject();
+            RenameParameterRenamePackageCell.checkCombination(dispatcherRefactoring, this.refactoringObject);
+            dispatcher.setRefactoringObject(dispatcherRefactoring);
         }
     }
 
