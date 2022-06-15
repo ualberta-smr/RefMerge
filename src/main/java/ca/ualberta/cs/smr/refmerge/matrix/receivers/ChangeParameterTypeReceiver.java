793fc5e70806cc3c25de0ac4d4102f1c369e04eb
Max Ellis <mjellis@ualberta.ca>
2022-06-15 15:48:11 -0700

    Add change parameter type / inline method combination check and dispatch

--- src/main/java/ca/ualberta/cs/smr/refmerge/matrix/receivers/ChangeParameterTypeReceiver.java
+++ src/main/java/ca/ualberta/cs/smr/refmerge/matrix/receivers/ChangeParameterTypeReceiver.java
@@ -1,13 +1,7 @@
 package ca.ualberta.cs.smr.refmerge.matrix.receivers;
 
-import ca.ualberta.cs.smr.refmerge.matrix.dispatcher.ExtractMethodDispatcher;
-import ca.ualberta.cs.smr.refmerge.matrix.dispatcher.MoveRenameMethodDispatcher;
-import ca.ualberta.cs.smr.refmerge.matrix.dispatcher.PullUpMethodDispatcher;
-import ca.ualberta.cs.smr.refmerge.matrix.dispatcher.PushDownMethodDispatcher;
-import ca.ualberta.cs.smr.refmerge.matrix.logicCells.ChangeParameterTypeExtractMethodCell;
-import ca.ualberta.cs.smr.refmerge.matrix.logicCells.ChangeParameterTypeMoveRenameMethodCell;
-import ca.ualberta.cs.smr.refmerge.matrix.logicCells.ChangeParameterTypePullUpMethodCell;
-import ca.ualberta.cs.smr.refmerge.matrix.logicCells.ChangeParameterTypePushDownMethodCell;
+import ca.ualberta.cs.smr.refmerge.matrix.dispatcher.*;
+import ca.ualberta.cs.smr.refmerge.matrix.logicCells.*;
 import ca.ualberta.cs.smr.refmerge.refactoringObjects.RefactoringObject;
 
 public class ChangeParameterTypeReceiver extends Receiver {
@@ -36,6 +30,20 @@ public class ChangeParameterTypeReceiver extends Receiver {
             this.isTransitive = false;
             // There is no opportunity for transitivity in this case. There is only a combination case that can occur
             ChangeParameterTypeExtractMethodCell.checkCombination(dispatcherRefactoring, this.refactoringObject);
+            dispatcher.setRefactoringObject(dispatcherRefactoring);
+        }
+    }
+
+    /*
+     * Check for change parameter type / inline method combination only to add to inline method resilience
+     */
+    @Override
+    public void receive(InlineMethodDispatcher dispatcher) {
+        RefactoringObject dispatcherRefactoring = dispatcher.getRefactoringObject();
+        if(dispatcher.isSimplify()) {
+            this.isTransitive = false;
+            // There is no opportunity for transitivity in this case. There is only a combination case that can occur
+            ChangeParameterTypeInlineMethodCell.checkCombination(dispatcherRefactoring, this.refactoringObject);
             dispatcher.setRefactoringObject(dispatcherRefactoring);
         }
     }
