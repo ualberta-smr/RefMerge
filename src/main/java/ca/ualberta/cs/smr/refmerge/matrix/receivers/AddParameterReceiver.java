0f089de870da87a3127b7d36272321bc5cd5c33e
Max Ellis <mjellis@ualberta.ca>
2022-06-15 10:12:57 -0700

    Add combination check for add parameter / extract method and dispatch

--- src/main/java/ca/ualberta/cs/smr/refmerge/matrix/receivers/AddParameterReceiver.java
+++ src/main/java/ca/ualberta/cs/smr/refmerge/matrix/receivers/AddParameterReceiver.java
@@ -1,6 +1,8 @@
 package ca.ualberta.cs.smr.refmerge.matrix.receivers;
 
+import ca.ualberta.cs.smr.refmerge.matrix.dispatcher.ExtractMethodDispatcher;
 import ca.ualberta.cs.smr.refmerge.matrix.dispatcher.MoveRenameMethodDispatcher;
+import ca.ualberta.cs.smr.refmerge.matrix.logicCells.AddParameterExtractMethodCell;
 import ca.ualberta.cs.smr.refmerge.matrix.logicCells.AddParameterMoveRenameMethodCell;
 import ca.ualberta.cs.smr.refmerge.refactoringObjects.RefactoringObject;
 
@@ -16,6 +18,20 @@ public class AddParameterReceiver extends Receiver {
             this.isTransitive = false;
             // There is no opportunity for transitivity in this case. There is only a combination case that can occur
             AddParameterMoveRenameMethodCell.checkCombination(dispatcherRefactoring, this.refactoringObject);
+            dispatcher.setRefactoringObject(dispatcherRefactoring);
+        }
+    }
+
+    /*
+     * Check for add parameter / extract method combination only to add to move + rename method resilience
+     */
+    @Override
+    public void receive(ExtractMethodDispatcher dispatcher) {
+        RefactoringObject dispatcherRefactoring = dispatcher.getRefactoringObject();
+        if(dispatcher.isSimplify()) {
+            this.isTransitive = false;
+            // There is no opportunity for transitivity in this case. There is only a combination case that can occur
+            AddParameterExtractMethodCell.checkCombination(dispatcherRefactoring, this.refactoringObject);
             dispatcher.setRefactoringObject(dispatcherRefactoring);
         }
     }
