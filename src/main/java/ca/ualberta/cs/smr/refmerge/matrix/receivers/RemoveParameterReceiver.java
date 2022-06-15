afa243f1c2f7b0fca1c4ec6f708abb2f7daf66d6
Max Ellis <mjellis@ualberta.ca>
2022-06-15 11:50:15 -0700

    Add remove parameter / inline method combination check

--- src/main/java/ca/ualberta/cs/smr/refmerge/matrix/receivers/RemoveParameterReceiver.java
+++ src/main/java/ca/ualberta/cs/smr/refmerge/matrix/receivers/RemoveParameterReceiver.java
@@ -1,8 +1,10 @@
 package ca.ualberta.cs.smr.refmerge.matrix.receivers;
 
 import ca.ualberta.cs.smr.refmerge.matrix.dispatcher.ExtractMethodDispatcher;
+import ca.ualberta.cs.smr.refmerge.matrix.dispatcher.InlineMethodDispatcher;
 import ca.ualberta.cs.smr.refmerge.matrix.dispatcher.MoveRenameMethodDispatcher;
 import ca.ualberta.cs.smr.refmerge.matrix.logicCells.RemoveParameterExtractMethodCell;
+import ca.ualberta.cs.smr.refmerge.matrix.logicCells.RemoveParameterInlineMethodCell;
 import ca.ualberta.cs.smr.refmerge.matrix.logicCells.RemoveParameterMoveRenameMethodCell;
 import ca.ualberta.cs.smr.refmerge.refactoringObjects.RefactoringObject;
 
@@ -23,7 +25,7 @@ public class RemoveParameterReceiver extends Receiver {
     }
 
     /*
-     * Check for remove parameter / move + rename method combination only to add to move + rename method resilience
+     * Check for remove parameter / extract method combination only to add to extract method resilience
      */
     @Override
     public void receive(ExtractMethodDispatcher dispatcher) {
@@ -32,6 +34,20 @@ public class RemoveParameterReceiver extends Receiver {
             this.isTransitive = false;
             // There is no opportunity for transitivity in this case. There is only a combination case that can occur
             RemoveParameterExtractMethodCell.checkCombination(dispatcherRefactoring, this.refactoringObject);
+            dispatcher.setRefactoringObject(dispatcherRefactoring);
+        }
+    }
+
+    /*
+     * Check for remove parameter / inline method combination only to add to inline method resilience
+     */
+    @Override
+    public void receive(InlineMethodDispatcher dispatcher) {
+        RefactoringObject dispatcherRefactoring = dispatcher.getRefactoringObject();
+        if(dispatcher.isSimplify()) {
+            this.isTransitive = false;
+            // There is no opportunity for transitivity in this case. There is only a combination case that can occur
+            RemoveParameterInlineMethodCell.checkCombination(dispatcherRefactoring, this.refactoringObject);
             dispatcher.setRefactoringObject(dispatcherRefactoring);
         }
     }
